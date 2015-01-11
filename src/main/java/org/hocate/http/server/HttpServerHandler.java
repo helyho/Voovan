cbfc5aef093f4a96c8589a0f47a72450b10d4d34
helyho <helyho@gmail.com>
2015-01-12 00:30:40 +0800

    对 Connection: keep-alive 的支持

--- src/main/java/org/hocate/http/server/HttpServerHandler.java
+++ src/main/java/org/hocate/http/server/HttpServerHandler.java
@@ -8,15 +8,17 @@ import org.hocate.tools.TObject;
 
 /**
  * HttpServer 业务处理类
+ * 
  * @author helyho
  *
  */
 public class HttpServerHandler implements IoHandler {
-	private RequestDispatch processer;
-	public HttpServerHandler(RequestDispatch processer){
+	private RequestDispatch	processer;
+
+	public HttpServerHandler(RequestDispatch processer) {
 		this.processer = processer;
 	}
-	
+
 	@Override
 	public Object onConnect(IoSession session) {
 		return null;
@@ -29,41 +31,44 @@ public class HttpServerHandler implements IoHandler {
 
 	@Override
 	public Object onReceive(IoSession session, Object obj) {
-		//构造请求
+		// 构造请求
 		HttpRequest request = TObject.cast(obj);
-		//构造响应报文并返回
+		// 构造响应报文并返回
 		HttpResponse response = new HttpResponse();
-		
+
 		try {
 			processer.Process(request, response);
+			session.setAttribute("isKeepAlive", request.header().get("Connection"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-//		System.out.println(request);
-//		System.out.println("======================================");
-//		try{
-//			for(Part part : request.parts()){
-//				System.out.println(part.getType());
-//				if(part.getType()==PartType.BINARY){
-//					part.saveAsFile("/Users/helyho/response.jpg");
-//				}
-//			}
-//		}
-//		catch(Exception e){
-//			e.printStackTrace();
-//		}
-		
-		
-//		String bodyContent = "test body content";
-//		response.body().setBody(bodyContent.getBytes());
-//		System.out.println("=================================");
-//		System.out.println(response);
+		// System.out.println(request);
+		// System.out.println("======================================");
+		// try{
+		// for(Part part : request.parts()){
+		// System.out.println(part.getType());
+		// if(part.getType()==PartType.BINARY){
+		// part.saveAsFile("/Users/helyho/response.jpg");
+		// }
+		// }
+		// }
+		// catch(Exception e){
+		// e.printStackTrace();
+		// }
+
+		// String bodyContent = "test body content";
+		// response.body().setBody(bodyContent.getBytes());
+		// System.out.println("=================================");
+		// System.out.println(response);
 		return response;
 	}
 
 	@Override
 	public void onSent(IoSession session, Object obj) {
-		// session.close();
+		String isKeepAlive = session.getAttribute("isKeepAlive").toString();
+		if (!isKeepAlive.equals("keep-alive")) {
+			session.close();
+		}
 	}
 
 	@Override
