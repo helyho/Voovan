85bf166b8e9115b3d4ab9433188a196e6acae83c
helyho <helyho@gmail.com>
2015-04-29 17:17:58 +0800

    优化 Http 消息分段算发

--- src/main/java/org/voovan/network/messageparter/HttpMessageParter.java
+++ src/main/java/org/voovan/network/messageparter/HttpMessageParter.java
@@ -9,25 +9,28 @@ import org.voovan.tools.TString;
 
 public class HttpMessageParter implements MessageParter {
 
-	private static final String BODY_TAG= "\r\n\r\n";
-	
+	private static final String	BODY_TAG	= "\r\n\r\n";
+
 	@Override
 	public boolean canPartition(IoSession session, byte[] buffer, int elapsedtime) {
-		
-		if(isHttpFrame(buffer)){
+
+		if (isHttpFrame(buffer)) {
 			return true;
-		}else if(isWebSocketFrame(ByteBuffer.wrap(buffer))){
+		} else if (isWebSocketFrame(ByteBuffer.wrap(buffer))) {
 			return true;
 		}
 
 		return false;
 	}
-	
-	public static boolean isHttpFrame(byte[] buffer){
+
+	public static boolean isHttpFrame(byte[] buffer) {
 		String bufferString = new String(buffer);
-		// 包含\r\n\r\n,这个时候 Content-Length 可能存在
+		String[] boundaryLines 		= TString.searchByRegex(bufferString, "boundary=[^ \\r\\n]+");
+		String[] contentLengthLines = TString.searchByRegex(bufferString, "Content-Length: .+[^\\r\\n]");
+
+		// 包含\r\n\r\n,这个时候报文有可能加载完成
 		if (bufferString.contains(BODY_TAG)) {
-			String[] contentLengthLines = TString.searchByRegex(bufferString, "Content-Length: .+[^\\r\\n]");
+
 			// 1.包含 content Length 的则通过获取 contentLenght 来计算报文的总长度,长度相等时,返回成功
 			if (contentLengthLines.length == 1) {
 				int contentLength = Integer.valueOf(contentLengthLines[0].split(" ")[1]);
@@ -36,85 +39,83 @@ public class HttpMessageParter implements MessageParter {
 					return true;
 				}
 			}
-			// 2.不包含 ContentLength头的报文,则通过\r\n\r\n进行结尾判断,
-			else if (bufferString.endsWith(BODY_TAG)) {
-				// 3.分段传输的 POST 请求报文的报文头和报文题结束标识都是\r\n\r\n,所以还要判断出现两次的\r\n\r\n 的位置不同说明报文加载完成
-				if (bufferString.indexOf(BODY_TAG) != bufferString.lastIndexOf(BODY_TAG)) {
-					// 如果是 post multipart/form-data类型,且没有指定
-					// ContentLength,则需要使用--boundary--的结尾形式来判断
-					String[] boundaryLines = TString.searchByRegex(bufferString, "boundary=[^ \\r\\n]+");
-					if (boundaryLines.length == 1 && bufferString.trim().endsWith("--" + boundaryLines[0] + "--")) {
-						return true;
-					} else {
-						return false;
-					}
-				}
-				// 4.不包含 ContentLength 头的报文,且没有 body 则返回成功,例如:GET,TRACE,OPTIONS 等请求
-				else {
-					return true;
-				} 
+			// 2. 如果是 HTTP 请求报文
+			//    POST方法的multipart/form-data类型,且没有指定ContentLength,则需要使用--boundary--的结尾形式来判断
+			else if (boundaryLines.length == 1 && bufferString.trim().endsWith("--" + boundaryLines[0] + "--")) {
+				return true;
 			}
+			// 3. 如果是 HTTP 响应报文 chunk
+			//    则trim 后判断最后一个字符是否是 0
+			else if (boundaryLines.length == 0 && bufferString.trim().endsWith("0")) {
+				return true;
+			}
+			// 4 HEAD,CONNECT,DELETE,GET,TRACE,OPTIONS等请求,没有报文内容
+			else if (bufferString.startsWith("GET") || bufferString.startsWith("TRACE") || bufferString.startsWith("OPTIONS")
+					|| bufferString.startsWith("HEAD") || bufferString.startsWith("DELETE") || bufferString.startsWith("CONNECT")) {
+				return true;
+			}
+
 		}
-		
+
 		return false;
-	}	
-	
-	public static boolean isWebSocketFrame( ByteBuffer buffer ) {
-		//接受数据的大小
+	}
+
+	public static boolean isWebSocketFrame(ByteBuffer buffer) {
+		// 接受数据的大小
 		int maxpacketsize = buffer.remaining();
 		// 期望数据包的实际大小
 		int expectPackagesize = 2;
-		if( maxpacketsize < expectPackagesize ){
+		if (maxpacketsize < expectPackagesize) {
 			return false;
 		}
 		byte finByte = buffer.get();
 		boolean fin = finByte >> 8 != 0;
-		byte rsv = (byte) ( ( finByte & ~(byte) 128 ) >> 4 );
-		if(rsv!=0){
+		byte rsv = (byte) ((finByte & ~(byte) 128) >> 4);
+		if (rsv != 0) {
 			return false;
 		}
 		byte maskByte = buffer.get();
-		boolean mask = ( maskByte & -128 ) != 0;
-		int payloadlength = (byte) ( maskByte & ~(byte) 128 );
-		int optcode =  (byte) ( finByte & 15 ) ;
+		boolean mask = (maskByte & -128) != 0;
+		int payloadlength = (byte) (maskByte & ~(byte) 128);
+		int optcode = (byte) (finByte & 15);
 
-		if( !fin ) {
-			if( optcode == 9 || optcode == 10 || optcode == 8 ) {
+		if (!fin) {
+			if (optcode == 9 || optcode == 10 || optcode == 8) {
 				return false;
 			}
 		}
 
-		if( payloadlength >= 0 && payloadlength <= 125 ) {
+		if (payloadlength >= 0 && payloadlength <= 125) {
 		} else {
-			if( optcode == 9 || optcode == 10 || optcode == 8 ) {
+			if (optcode == 9 || optcode == 10 || optcode == 8) {
 				return false;
 			}
-			if( payloadlength == 126 ) {
-				expectPackagesize += 2; 
-				byte[] sizebytes = new byte[ 3 ];
-				sizebytes[ 1 ] = buffer.get();
-				sizebytes[ 2 ] = buffer.get();
-				payloadlength = new BigInteger( sizebytes ).intValue();
+			if (payloadlength == 126) {
+				expectPackagesize += 2;
+				byte[] sizebytes = new byte[3];
+				sizebytes[1] = buffer.get();
+				sizebytes[2] = buffer.get();
+				payloadlength = new BigInteger(sizebytes).intValue();
 			} else {
-				expectPackagesize += 8; 
-				byte[] bytes = new byte[ 8 ];
-				for( int i = 0 ; i < 8 ; i++ ) {
-					bytes[ i ] = buffer.get();
+				expectPackagesize += 8;
+				byte[] bytes = new byte[8];
+				for (int i = 0; i < 8; i++) {
+					bytes[i] = buffer.get();
 				}
-				long length = new BigInteger( bytes ).longValue();
-				if( length <= Integer.MAX_VALUE ) {
+				long length = new BigInteger(bytes).longValue();
+				if (length <= Integer.MAX_VALUE) {
 					payloadlength = (int) length;
 				}
 			}
 		}
 
-		expectPackagesize += ( mask ? 4 : 0 );
+		expectPackagesize += (mask ? 4 : 0);
 		expectPackagesize += payloadlength;
 
-		//如果世界接受的数据小于数据包的大小则报错
-		if( maxpacketsize < expectPackagesize ){
+		// 如果实际接受的数据小于数据包的大小则报错
+		if (maxpacketsize < expectPackagesize) {
 			return false;
-		}else{
+		} else {
 			return true;
 		}
 	}
