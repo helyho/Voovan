dd5620ed213f13f131a40ec40e10dc83e2e65f6e
helyho <helyho@gmail.com>
2015-05-08 12:09:02 +0800

    对请求参数,在构造时自动使用字符集参数进行编码.

--- src/test/java/org/voovan/test/http/HttpClientUnit.java
+++ src/test/java/org/voovan/test/http/HttpClientUnit.java
@@ -27,7 +27,7 @@ public class HttpClientUnit extends TestCase {
 	}
 	
 //	public void testGet() throws Exception{
-//		HttpClient getClient = new HttpClient("http://127.0.0.1:28080","GB2312");
+//		HttpClient getClient = new HttpClient("http://webserver.voovan.com","GB2312");
 //		getClient.setMethod("GET");
 //		getClient.putParameters("name", "测试");
 //		Response response = getClient.Connect();
@@ -36,22 +36,24 @@ public class HttpClientUnit extends TestCase {
 //	}
 
 //	public void testPost() throws Exception {
-//		HttpClient postClient = new HttpClient("http://dig.chouti.com/login");
+//		HttpClient postClient = new HttpClient("http://webserver.voovan.com","GB2312");
 //		postClient.setMethod("POST");
-//		postClient.putParameters("jid", "helyho");
-//		postClient.putParameters("password", "****");
+//		postClient.putParameters("name", "测试");
+//		postClient.putParameters("age", "32");
 //		Response response = postClient.Connect();
-//		Logger.simple(response.body().getBodyString());
+//		Logger.simple(response.body().getBodyString("GB2312"));
 //		assertTrue(response.protocol().getStatus() != 500);
 //	}
 	
 	public void testMultiPart() throws Exception {
 		HttpClient mpClient = new HttpClient("http://127.0.0.1:28080");
 		mpClient.setMethod("POST");
-		Part part = new Part();
-		part.header().put("name", "name");
-		part.body().write("测试","GB2312");
-		mpClient.addPart(part);
+		
+		Part partName = new Part("name","测试","GB2312");
+		Part partAge = new Part("age","23","GB2312");
+		
+		mpClient.addPart(partName);
+		mpClient.addPart(partAge);
 		
 		Response response = mpClient.Connect();
 		Logger.simple(response.body().getBodyString("GB2312"));
