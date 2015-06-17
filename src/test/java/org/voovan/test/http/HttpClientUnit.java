66bce65184fa2d51790de07f0b0627922345cb48
helyho <helyho@gmail.com>
2015-06-17 14:33:03 +0800

    将 HTTPClient 修改成可以不关闭连接持续不断的返送 HTTP 请求.

--- src/test/java/org/voovan/test/http/HttpClientUnit.java
+++ src/test/java/org/voovan/test/http/HttpClientUnit.java
@@ -15,13 +15,13 @@ public class HttpClientUnit extends TestCase {
 	}
 
 	public void testGetHeader() {
-		HttpClient httpClient = new HttpClient("http://127.0.0.1:28080");
+		HttpClient httpClient = new HttpClient("http://webserver.voovan.com");
 		httpClient.putParameters("name", "测试");
-		assertEquals(httpClient.getHeader().get("Host"),"127.0.0.1");
+		assertEquals(httpClient.getHeader().get("Host"),"webserver.voovan.com");
 	}
 
 	public void testParameters() {
-		HttpClient httpClient = new HttpClient("http://127.0.0.1:28080");
+		HttpClient httpClient = new HttpClient("http://webserver.voovan.com");
 		httpClient.putParameters("name", "测试");
 		assertEquals(httpClient.getParameters().get("name"), "测试");
 	}
@@ -30,25 +30,27 @@ public class HttpClientUnit extends TestCase {
 		HttpClient getClient = new HttpClient("http://webserver.voovan.com","GB2312");
 		Response response  = getClient.setMethod("GET")
 			.putParameters("name", "测试")
-			.putParameters("age", "32").connect();
+			.putParameters("age", "32").send();
 		Logger.simple(response.body().getBodyString("GB2312"));
 		assertTrue(response.protocol().getStatus()!=500);
+		getClient.close();
 	}
 
 	public void testPost() throws Exception {
-	HttpClient postClient = new HttpClient("http://webserver.voovan.com","GB2312");
-	Response response = postClient.setMethod("POST") 
-		.putParameters("name", "测试")
-		.putParameters("age", "32").connect();
-	Logger.simple(response.body().getBodyString("GB2312"));
-	assertTrue(response.protocol().getStatus() != 500);
+		HttpClient postClient = new HttpClient("http://webserver.voovan.com","GB2312");
+		Response response = postClient.setMethod("POST") 
+			.putParameters("name", "测试")
+			.putParameters("age", "32").send();
+		Logger.simple(response.body().getBodyString("GB2312"));
+		assertTrue(response.protocol().getStatus() != 500);
+		postClient.close();
 	}
 	
 	public void testMultiPart() throws Exception {
 		HttpClient mpClient = new HttpClient("http://webserver.voovan.com");
 		Response response = mpClient.setMethod("POST")
 			.addPart(new Part("name","测试","GB2312"))
-			.addPart(new Part("age","23","GB2312")).connect();
+			.addPart(new Part("age","23","GB2312")).send();
 		
 		Logger.simple(response.body().getBodyString("GB2312"));
 		assertTrue(response.protocol().getStatus()!=500);
