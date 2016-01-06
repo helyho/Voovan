9c04c60fee84c4139189c742f540bc7ab9d8f636
helyho <helyho@gmail.com>
2016-01-06 11:46:00 +0800

    解决发送是缓冲区满,后续字节没有继续发送的 bug

--- Config/web.js
+++ Config/web.js
@@ -6,7 +6,7 @@
 	// 连接超时时间(s),默认30秒
 	"Timeout"			: 30,
 	// 上下文路径
-	"ContextPath"		: "/Users/helyho/Downloads",
+	"ContextPath"		: "/Users/helyho/Downloads",// /home/helyho/HttpServerDemo/WebApp
 	// 默认字符集,默认 UTF-8
 	"CharacterSet"		: "GB2312",
 	// Session 容器类,默认java.util.Hashtable
