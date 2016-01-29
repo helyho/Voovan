199e1d8a9ff0f4d54e3a0e25c229e23eecf46cf9
helyho <helyho@gmail.com>
2016-01-30 04:37:45 +0800

    逐步优化

--- Config/web.js
+++ Config/web.js
@@ -16,7 +16,7 @@
 	// KeepAlive 超时时间(s),默认5秒 (该参数同样会被应用到 WebSocket 的连接保持上)
 	"KeepAliveTimeout"  : 5,
 	// 是否启用Gzip压缩
-	"Gzip"              : "on",
+	"Gzip"              : "off",
 	// 过滤器 先执行filter1, 后执行filter2
 	/*
 	"Filter":[
