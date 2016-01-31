8d5d2b3e9731725cf401b2c9722ddc0216d5a6db
helyho <helyho@gmail.com>
2016-01-31 21:11:09 +0800

    增加异步通信框架通过 http 压测工作测试压力的测试程序

---
+++ src/test/java/org/voovan/test/network/performTest/PerformTestSpliter.java
@@ -0,0 +1,18 @@
+package org.voovan.test.network.performTest;
+
+import org.voovan.network.IoSession;
+import org.voovan.network.MessageSplitter;
+
+public class PerformTestSpliter implements MessageSplitter {
+
+	@Override
+	public boolean canSplite(IoSession session, byte[] buffer, int elapsedtime) {
+		String requestStr = new String(buffer);
+		if(requestStr.endsWith("\r\n\r\n")){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	 
+}
