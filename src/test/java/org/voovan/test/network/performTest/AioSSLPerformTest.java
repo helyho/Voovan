1d38e54bdfe0a802fab2337a63e5f32c1df50cc5
helyho <helyho@gmail.com>
2016-02-14 14:50:17 +0800

    增加 HTTPS 浏览器(SSL/TLS) 的测试用例

---
+++ src/test/java/org/voovan/test/network/performTest/AioSSLPerformTest.java
@@ -0,0 +1,22 @@
+package org.voovan.test.network.performTest;
+
+import java.io.IOException;
+
+import org.voovan.network.SSLManager;
+import org.voovan.network.aio.AioServerSocket;
+import org.voovan.network.filter.StringFilter;
+
+public class AioSSLPerformTest  {
+
+	public static void main(String[] args) throws IOException {
+		SSLManager sslManager = new SSLManager("TLS",false);
+		sslManager.loadCertificate(System.getProperty("user.dir")+"/src/test/java/org/voovan/test/network/ssl/ssl_ks", "passStr","123123");
+		
+		AioServerSocket serverSocket = new AioServerSocket("0.0.0.0",28081,500);
+		serverSocket.setSSLManager(sslManager);
+		serverSocket.handler(new PerformTestHandler());
+		serverSocket.filterChain().add(new StringFilter());
+		serverSocket.messageSplitter(new PerformTestSpliter());
+		serverSocket.start();
+	}
+}
