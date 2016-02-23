749845b33a7214ebb6e3881437d259f135a9016b
helyho <helyho@gmail.com>
2016-02-23 14:54:13 +0800

    增加 SSL 连接 无客户端认证模式

--- src/test/java/org/voovan/test/network/ssl/NioSSLClient.java
+++ src/test/java/org/voovan/test/network/ssl/NioSSLClient.java
@@ -11,7 +11,7 @@ public class NioSSLClient {
 		SSLManager sslManager = new SSLManager("SSL");
 		sslManager.loadCertificate(System.getProperty("user.dir")+"/src/test/java/org/voovan/test/network/ssl/ssl_ks", "passStr","123123");
 		
-		NioSocket socket = new NioSocket("127.0.0.1",2031,500);
+		NioSocket socket = new NioSocket("127.0.0.1",2031,1000);
 		socket.setSSLManager(sslManager);
 		socket.handler(new ClientHandlerTest());
 		socket.filterChain().add(new StringFilter());
