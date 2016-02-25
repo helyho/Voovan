0fef82b6d1df68c496e3f76d52c13fccc026c25b
helyho <helyho@gmail.com>
2016-02-25 14:08:50 +0800

    优化

--- src/main/java/org/voovan/network/messagesplitter/HttpMessageSplitter.java
+++ src/main/java/org/voovan/network/messagesplitter/HttpMessageSplitter.java
@@ -35,15 +35,15 @@ public class HttpMessageSplitter implements MessageSplitter {
 	public static boolean isHttpFrame(byte[] buffer) {
 		String bufferString = new String(buffer);
 		String[] boundaryLines = TString.searchByRegex(bufferString, "boundary=[^ \\r\\n]+");
-		String[] contentLengthLines = TString.searchByRegex(bufferString, "Content-Length: .+[^\\r\\n]");
+		String[] contentLengthLines = TString.searchByRegex(bufferString, "Content-Length: \\d+");
 
 		// 包含\r\n\r\n,这个时候报文有可能加载完成
-		if (bufferString.endsWith(BODY_TAG)) {
+		if (bufferString.contains(BODY_TAG)) {
 			// 1.包含 content Length 的则通过获取 contentLenght 来计算报文的总长度,长度相等时,返回成功
 			if (contentLengthLines.length == 1) {
 				int contentLength = Integer.valueOf(contentLengthLines[0].split(" ")[1].trim());
 				int totalLength = bufferString.indexOf(BODY_TAG) + 4 + contentLength;
-				if (buffer.length == totalLength) {
+				if (buffer.length >= totalLength) {
 					return true;
 				}
 			}
