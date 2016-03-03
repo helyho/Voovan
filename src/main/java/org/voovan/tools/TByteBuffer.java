89305409b7a26ecfb913f5ba26bd58f0868cbcb0
helyho <helyho@gmail.com>
2016-03-03 19:52:59 +0800

    优化日志方法

---
+++ src/main/java/org/voovan/tools/TByteBuffer.java
@@ -0,0 +1,28 @@
+package org.voovan.tools;
+
+import java.nio.ByteBuffer;
+
+/**
+ * 类文字命名
+ *
+ * @author helyho
+ *         <p>
+ *         Java Framework.
+ *         WebSite: https://github.com/helyho/Voovan
+ *         Licence: Apache v2 License
+ */
+public class TByteBuffer {
+
+    public static byte[] toArray(ByteBuffer bytebuffer){
+        if(!bytebuffer.hasArray()) {
+            int oldPosition = bytebuffer.position();
+            bytebuffer.position(0);
+            int size = bytebuffer.limit();
+            byte[] buffers = new byte[size];
+            bytebuffer.get(buffers);
+            return buffers;
+        }else{
+            return bytebuffer.array();
+        }
+    }
+}
