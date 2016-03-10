900a407c5536986867e5df44289a809cad54c317
helyho <helyho@gmail.com>
2016-03-11 01:03:28 +0800

    日志输出性能优化 #20

--- src/test/java/org/voovan/test/Other.java
+++ src/test/java/org/voovan/test/Other.java
@@ -1,12 +1,14 @@
 package org.voovan.test;
 
 import org.voovan.tools.TEnv;
+import org.voovan.tools.log.Logger;
 
 
 public class Other {
 	public static void main(String[] args) throws Exception {
-		while(true){
-			TEnv.sleep(1);
+		for(int i=0;i<100;i++){
+			Logger.simple("Current count: "+i);
+			System.out.println("-------------------------------");
 		}
 	}
 	
