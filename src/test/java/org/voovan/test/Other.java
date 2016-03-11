a0a7898a4916f0ecdf8139e2dd2ab3ccb7b93ed0
helyho <helyho@gmail.com>
2016-03-11 16:13:19 +0800

    #21 HTTP的路径变量支持*匹配

--- src/test/java/org/voovan/test/Other.java
+++ src/test/java/org/voovan/test/Other.java
@@ -6,8 +6,11 @@ import org.voovan.tools.log.Logger;
 
 public class Other {
 	public static void main(String[] args) throws Exception {
+
 		for(int i=0;i<100;i++){
-			Logger.simple("Current count: "+i);
+			Logger.simple("Current count: " + i +"-1");
+			Logger.simple("Current count: " + i +"-2");
+			Logger.simple("Current count: " + i +"-3");
 			System.out.println("-------------------------------");
 		}
 	}
