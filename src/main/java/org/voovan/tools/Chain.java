b5441905029dd41d72308dfdc7b74eabf461dd0e
helyho <helyho@gmail.com>
2016-03-11 17:08:20 +0800

    #22 多个filter执行顺序 1.Socket 异步通讯的顺序问题 2.http 过滤器顺序问题

--- src/main/java/org/voovan/tools/Chain.java
+++ src/main/java/org/voovan/tools/Chain.java
@@ -15,6 +15,7 @@ public class Chain<E> extends ArrayDeque<E>{
 	
 	private static final long serialVersionUID = -4820686406224770808L;	
 	private Iterator<E> iterator;
+	private Iterator<E> invertedIterator;
 	private boolean isStop;
 	private E currentObj;
 	
@@ -32,6 +33,7 @@ public class Chain<E> extends ArrayDeque<E>{
 	public void rewind(){
 		isStop = false;
 		iterator = this.iterator();
+		invertedIterator = this.descendingIterator();
 	}
 	
 	/**
@@ -40,7 +42,15 @@ public class Chain<E> extends ArrayDeque<E>{
 	public void stop(){
 		this.isStop = true;
 	}
-	
+
+	/**
+	 * 迭代器当前元素
+	 * @return
+	 */
+	public E current(){
+		return currentObj;
+	}
+
 	/**
 	 * 迭代器下一个元素
 	 * @return
@@ -57,15 +67,8 @@ public class Chain<E> extends ArrayDeque<E>{
 			}
 		}
 	}
-	
-	/**
-	 * 迭代器当前元素
-	 * @return
-	 */
-	public E current(){
-		return currentObj;
-	}
-	
+
+
 	/**
 	 * 迭代器是否有下一个对象
 	 * @return
@@ -75,6 +78,37 @@ public class Chain<E> extends ArrayDeque<E>{
 			return false;
 		} else {
 			return iterator.hasNext();
+		}
+	}
+
+
+
+	/**
+	 * 迭代器上一个元素
+	 * @return
+	 */
+	public E previous(){
+		if(isStop){
+			return null;
+		} else {
+			if(invertedIterator.hasNext()){
+				currentObj = invertedIterator.next();
+				return currentObj;
+			} else {
+				return null;
+			}
+		}
+	}
+
+	/**
+	 * 迭代器是否有上一个对象
+	 * @return
+	 */
+	public boolean hasPrevious(){
+		if(isStop){
+			return false;
+		} else {
+			return invertedIterator.hasNext();
 		}
 	}
 	
