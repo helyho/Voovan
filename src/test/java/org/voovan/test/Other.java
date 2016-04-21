3aa447552bc707430e009dc2d41a1cf9856b72e0
helyho <helyho@gmail.com>
2016-04-21 12:26:54 +0800

    TFile 中增加读取文件最后几行的行数 public static byte[] loadFileLastLines(File file, int lastLineNum) throws IOException

--- src/test/java/org/voovan/test/Other.java
+++ src/test/java/org/voovan/test/Other.java
@@ -6,6 +6,7 @@ import org.voovan.tools.TPerformance;
 import org.voovan.tools.TStream;
 import org.voovan.tools.log.Logger;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.ParameterizedType;
@@ -17,24 +18,7 @@ public class Other {
     public static Map<String,List> alist = new HashMap<String,List>();
 
     public static void main(String[] args) throws Exception {
-        System.out.println(alist.getClass().getGenericSuperclass().getTypeName());
-        ParameterizedType pt = (ParameterizedType) alist.getClass().getGenericSuperclass();
-        System.out.println(pt.getActualTypeArguments().length);
-        System.out.println(pt.getActualTypeArguments()[0]);
-        System.out.println(pt.getActualTypeArguments()[1]);
-
-        Properties x =  System.getProperties();
-        for(Map.Entry<Object,Object> m: x.entrySet()){
-            if(!m.getKey().equals("line.separator")) {
-                System.out.println(m.getKey() + "=" + m.getValue());
-            }else{
-                byte[] bytes = m.getValue().toString().getBytes();
-                System.out.println(m.getKey() + "=" + (bytes.length>=1?bytes[0]:"") + "" +(bytes.length>=2?bytes[1]:"") );
-            }
-        }
-
-        TPerformance.getSysObjectInfo(3018,"");
-        TEnv.getThreads();
+        System.out.println(new String(TFile.loadFileLastLines(new File("/Users/helyho/Work/Java/Voovan/logs/sysout.20160419.log"),20)));
     }
 
 }
