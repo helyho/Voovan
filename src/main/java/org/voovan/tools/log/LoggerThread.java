62705225561b40e727a420abe5dec1d1bec102d6
helyho <helyho@gmail.com>
2016-06-27 19:23:15 +0800

    代码性能优化

--- src/main/java/org/voovan/tools/log/LoggerThread.java
+++ src/main/java/org/voovan/tools/log/LoggerThread.java
@@ -108,6 +108,8 @@ public class LoggerThread implements Runnable {
 				e.printStackTrace();
 			}
 		}
+
+		finished = true;
 	}
 	
 	/**
--- src/test/java/org/voovan/test/network/ssl/AioSSLClient.java
+++ src/test/java/org/voovan/test/network/ssl/AioSSLClient.java
@@ -3,6 +3,7 @@ package org.voovan.test.network.ssl;
 import org.voovan.network.SSLManager;
 import org.voovan.network.aio.AioSocket;
 import org.voovan.network.filter.StringFilter;
+import org.voovan.network.messagesplitter.LineMessageSplitter;
 import org.voovan.test.network.ClientHandlerTest;
 
 public class AioSSLClient {
@@ -11,10 +12,11 @@ public class AioSSLClient {
 		SSLManager sslManager = new SSLManager("SSL");
 		sslManager.loadCertificate(System.getProperty("user.dir")+"/src/test/java/org/voovan/test/network/ssl/ssl_ks", "passStr","123123");
 		
-		AioSocket socket = new AioSocket("127.0.0.1",2031,1000);
+		AioSocket socket = new AioSocket("127.0.0.1",2031,10000);
 		socket.setSSLManager(sslManager);
 		socket.handler(new ClientHandlerTest());
 		socket.filterChain().add(new StringFilter());
+		socket.messageSplitter(new LineMessageSplitter());
 		socket.start();
 	}
 }
