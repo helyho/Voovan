290b1877957c0d3d8722fc1d7d7c454609328dd3
helyho <helyho@gmail.com>
2016-07-01 15:12:32 +0800

    代码优化

---
+++ src/test/java/org/voovan/test/http/SimpleHttpServerDemo.java
@@ -0,0 +1,22 @@
+package org.voovan.test.http;
+
+import org.voovan.http.server.HttpServer;
+
+import java.io.IOException;
+
+/**
+ * Http简单服务测试类
+ *
+ * @author helyho
+ *
+ * Java Framework.
+ * WebSite: https://github.com/helyho/Voovan
+ * Licence: Apache v2 License
+ */
+public class SimpleHttpServerDemo {
+    public static void main(String[] args) {
+        HttpServer.newInstance(20001).get("/test",(req,resp)->{
+            resp.write("this is test Message.");
+        }).serve();
+    }
+}
