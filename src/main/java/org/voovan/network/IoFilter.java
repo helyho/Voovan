d4e1bf45ea5d14701aad3d0ffe1f20f8658a9a14
helyho <helyho@gmail.com>
2016-07-05 15:46:51 +0800

    1.Mime 为极少变动的静态信息,调整到应用jar的静态资源中.但同时保留了对自定义配置的支持,自定义配置会覆盖系统默认配置. 2.优化了 Error 的错误信息提示 3.为 HttpServer 增加了参数为 WebServerConfig 参数的静态方法.

--- src/main/java/org/voovan/network/IoFilter.java
+++ src/main/java/org/voovan/network/IoFilter.java
@@ -16,7 +16,7 @@ public interface IoFilter {
 	 * 过滤器解密函数,接收事件(onRecive)前调用
 	 * 			onRecive事件前调用
 	 * @param session  session 对象
-	 * @param object   解码对象
+	 * @param object   解码对象,上一个过滤器的返回值
 	 * @return 解码后对象
 	 * @throws IoFilterException 过滤器异常
 	 */
@@ -26,7 +26,7 @@ public interface IoFilter {
 	 * 过滤器加密函数,发送事件(onSend)前调用
 	 * 			send事件前调用
 	 * @param session 	session 对象
-	 * @param object    编码对象
+	 * @param object    编码对象,上一个过滤器的返回值
 	 * @return 编码后对象
 	 * @throws IoFilterException 过滤器异常
 	 */
