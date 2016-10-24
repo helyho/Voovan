12cbf0438d1747f72a7ce8ff9ab173c6c140bb39
helyho <helyho@gmail.com>
2016-10-24 15:06:26 +0800

    #45 增加 jsonpath 的访问支持

--- src/test/java/org/voovan/test/tools/json/JSONDecodeUnit.java
+++ src/test/java/org/voovan/test/tools/json/JSONDecodeUnit.java
@@ -3,6 +3,7 @@ package org.voovan.test.tools.json;
 import junit.framework.TestCase;
 import org.voovan.tools.TObject;
 import org.voovan.tools.json.JSONDecode;
+import org.voovan.tools.log.Logger;
 
 import java.util.List;
 import java.util.Map;
@@ -21,7 +22,7 @@ public class JSONDecodeUnit extends TestCase {
 								"\"string\":\"helyho\","+
 								"\"tb2\":{"+
 									"\"bint\":56,"+
-									"\"string\":\"bingo\","+
+									"\"string\":\"bi\\\"ngo\","+
 									"\"list\":["+
 										"\"tb2 list item\""+
 									"],"+
@@ -43,6 +44,7 @@ public class JSONDecodeUnit extends TestCase {
 										   "}, " +
 								"\"nullValue\":null"+
 							"}";
+
 		Map<String, Object> obj = TObject.cast(JSONDecode.parse(jsonString));
 		assertTrue((Integer)obj.get("bint")==32);
 		assertEquals((String)obj.get("string"),"helyho");
@@ -50,7 +52,7 @@ public class JSONDecodeUnit extends TestCase {
 		assertTrue(((Map)obj.get("map")).size() == 2);
 		Map<String, Object> tb2 = TObject.cast(obj.get("tb2"));
 		assertTrue((Integer)tb2.get("bint")==56);
-		assertEquals((String)tb2.get("string"),"bingo");
+		assertEquals((String)tb2.get("string"),"bi\\\"ngo");
 		assertTrue(((List)tb2.get("list")).size() == 1);
 		assertTrue(((Map)tb2.get("map")).size() == 1);
 		
@@ -60,7 +62,7 @@ public class JSONDecodeUnit extends TestCase {
 		assertTrue(object.getList().size() == 3);
 		assertTrue(object.getMap().size() == 2);
 		assertTrue(object.getTb2().getBint()==56);
-		assertEquals(object.getTb2().getString(),"bingo");
+		assertEquals(object.getTb2().getString(),"bi\\\"ngo");
 		assertTrue(object.getTb2().getList().size() == 1);
 		assertTrue(object.getTb2().getMap().size() == 1);
 
