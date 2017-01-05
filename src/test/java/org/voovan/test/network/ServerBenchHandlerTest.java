0585d6c39ae099163dfcc4057d15de041866975d
helyho <helyho@gmail.com>
2017-01-05 15:21:28 +0800

    imp: 优化TReflect.getObjectFromMap方法,异常信息增加提示

---
+++ src/test/java/org/voovan/test/network/ServerBenchHandlerTest.java
@@ -0,0 +1,49 @@
+package org.voovan.test.network;
+
+
+import org.voovan.network.IoHandler;
+import org.voovan.network.IoSession;
+import org.voovan.network.exception.SocketDisconnectByRemote;
+import org.voovan.tools.log.Logger;
+
+import java.nio.ByteBuffer;
+
+public class ServerBenchHandlerTest implements IoHandler {
+
+	@Override
+	public Object onConnect(IoSession session) {
+		return null;
+	}
+
+	@Override
+	public void onDisconnect(IoSession session) {
+	}
+
+	@Override
+	public Object onReceive(IoSession session, Object obj) {
+			return "HTTP/1.1 200 OK\r\n" +
+					"Server: Voovan-WebServer/V1.0-beta-3.2\r\n" +
+					"Connection: keep-alive\r\n" +
+					"Content-Length: 2\r\n" +
+					"Date: Thu, 05 Jan 2017 04:55:20 GMT\r\n" +
+					"Content-Type: text/html\r\n"+
+					"\r\n"+
+					"OK\r\n\n";
+		}
+
+		@Override
+		public void onException(IoSession session, Exception e) {
+			if(e instanceof SocketDisconnectByRemote){
+				Logger.simple("Connection disconnect by client");
+			}else {
+				Logger.error("Server Exception:",e);
+		}
+		session.close();
+	}
+
+	@Override
+	public void onSent(IoSession session, Object obj) {
+		session.close();
+	}
+
+}
