cc58f07e60e73681442e98e321ad42148636ca1f
helyho <helyho@gmail.com>
2017-01-06 02:53:31 +0800

    fix: 修复 JSON 解析嵌套注释时的 bug.

--- src/test/java/org/voovan/test/Other.java
+++ src/test/java/org/voovan/test/Other.java
@@ -1,14 +1,20 @@
 package org.voovan.test;
 
 import org.voovan.test.tools.json.TestObject;
+import org.voovan.tools.TEnv;
+import org.voovan.tools.TFile;
+import org.voovan.tools.TObject;
 import org.voovan.tools.json.JSON;
+import org.voovan.tools.json.JSONDecode;
 import org.voovan.tools.log.Logger;
 import org.voovan.tools.reflect.TReflect;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class Other {
@@ -23,9 +29,21 @@ public class Other {
         Logger.simple(TReflect.getMethodParameterGenericType(m,0)[0]);
 
         ByteBuffer bb = ByteBuffer.allocate(0);
-        bb.put("asdfadfasfd".getBytes());
 
-        Logger.info(new String(bb.array()));
+        Map<String, Object> WEB_CONFIG = loadMapFromFile("/conf/web.json");
+    }
 
+    private static Map<String, Object> loadMapFromFile(String filePath){
+        if(TFile.fileExists(TEnv.getSystemPath(filePath))) {
+            String fileContent = null;
+            try {
+                fileContent = new String(TFile.loadFileFromContextPath(filePath),"UTF-8");
+                Object configObject = JSONDecode.parse(fileContent);
+                return TObject.cast(configObject);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        return new HashMap<String,Object>();
     }
 }
