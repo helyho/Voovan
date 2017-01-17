5309c0e1e81621eb70b742cc6018c8a37664c4e9
helyho <helyho@gmail.com>
2017-01-18 04:11:39 +0800

    imp: MessageLoader 最终优化,除了数组扩容,所有的字节数组操作都使用 System.arrayCopy 来提高性能

--- src/test/java/org/voovan/test/network/aio/SyncAioSocketTest.java
+++ src/test/java/org/voovan/test/network/aio/SyncAioSocketTest.java
@@ -8,7 +8,7 @@ import org.voovan.tools.log.Logger;
 public class SyncAioSocketTest {
 	
 	public static void main(String[] args) throws Exception {
-		AioSocket socket = new AioSocket("127.0.0.1",2031,30000);
+		AioSocket socket = new AioSocket("127.0.0.1",2031,5000);
 		socket.filterChain().add(new StringFilter());
 		socket.messageSplitter(new LineMessageSplitter());
 		socket.syncStart();
