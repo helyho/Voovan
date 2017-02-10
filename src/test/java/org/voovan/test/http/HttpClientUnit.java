a2b280fb38589114fcc77ec9ba32c15c98ba2a9c
helyho <helyho@gmail.com>
2017-02-10 21:29:26 +0800

    fix: 修复服务端监听不存在时导致一直尝试连接服务端监听的 bug

--- src/test/java/org/voovan/test/http/HttpClientUnit.java
+++ src/test/java/org/voovan/test/http/HttpClientUnit.java
@@ -86,7 +86,7 @@ public class HttpClientUnit extends TestCase {
 	}
 
 	public void testWebSocket() throws Exception {
-		HttpClient httpClient = new HttpClient("ws://127.0.0.1:28080/","GBK2312",10000);
+		HttpClient httpClient = new HttpClient("ws://127.0.0.1:28080/","GBK2312",5);
 		httpClient.webSocket("/websocket", new WebSocketRouter() {
 
 			@Override
