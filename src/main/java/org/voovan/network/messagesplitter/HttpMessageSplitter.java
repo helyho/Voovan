4bcf41bdb92488da95a0aebacf2523f744180a2d
helyho <helyho@gmail.com>
2017-02-16 14:20:35 +0800

    imp: HttpMessageSplitter 性能优化 fix: MessageLoader 超时控制,原来是累计超时时间.现在修改为连续读数据异常才会超时.

--- src/main/java/org/voovan/network/messagesplitter/HttpMessageSplitter.java
+++ src/main/java/org/voovan/network/messagesplitter/HttpMessageSplitter.java
@@ -4,8 +4,12 @@ import org.voovan.http.websocket.WebSocketTools;
 import org.voovan.network.IoSession;
 import org.voovan.network.MessageSplitter;
 import org.voovan.tools.TByteBuffer;
+import org.voovan.tools.TStream;
 import org.voovan.tools.TString;
+import org.voovan.tools.log.Logger;
 
+import java.io.ByteArrayInputStream;
+import java.lang.reflect.Executable;
 import java.nio.ByteBuffer;
 
 /**
@@ -21,6 +25,7 @@ public class HttpMessageSplitter implements MessageSplitter {
 
 	private static final String	BODY_TAG	= "\r\n\r\n";
 	private int result = -1;
+	private int bodyTagIndex = -1;
 
 
 	@Override
@@ -36,61 +41,79 @@ public class HttpMessageSplitter implements MessageSplitter {
 			result = WebSocketTools.isWebSocketFrame(byteBuffer);
 		}
 
+		if(result!=-1){
+            bodyTagIndex = -1;
+        }
+
 		return result;
 	}
 
-	public static int isHttpFrame(ByteBuffer byteBuffer) {
-        String bufferString = TByteBuffer.toString(byteBuffer, "UTF-8");
-
-        if(bufferString.contains("\r\n")){
-            String firstLine = bufferString.substring(0,bufferString.indexOf("\r\n"));
-            if(TString.regexMatch(firstLine,"HTTP\\/\\d\\.\\d\\s\\d{3}\\s.*") <= 0){
-                if(TString.regexMatch(firstLine,"^[A-Z]*\\s.*\\sHTTP\\/\\d\\.\\d") <= 0){
-                    return -1;
-                }
+	public int getBodyTagIndex(ByteBuffer byteBuffer){
+        byte[] buffer = byteBuffer.array();
+        for(int x=0;x<buffer.length;x++){
+            if(buffer[x] == '\r' && buffer[x+1] == '\n' && buffer[x+2] == '\r' && buffer[x+3] == '\n'){
+                return x;
             }
         }
+        return -1;
+    }
 
-        String[] boundaryLines = TString.searchByRegex(bufferString, "boundary=[^ \\r\\n]+");
-        String[] contentLengthLines = TString.searchByRegex(bufferString, "Content-Length: \\d+");
-        boolean isChunked = bufferString.contains("chunked");
+	public int isHttpFrame(ByteBuffer byteBuffer) {
+	    try{
+            if(bodyTagIndex==-1) {
+                bodyTagIndex = getBodyTagIndex(byteBuffer);
+            }
 
-        // 包含\r\n\r\n,这个时候报文有可能加载完成
-//			if (bufferString.contains(BODY_TAG)){
-//				return bufferString.indexOf(BODY_TAG) + BODY_TAG.length();
-//			}
+            if(bodyTagIndex!=-1) {
 
-        if (bufferString.contains(BODY_TAG)) {
-            // 1.包含 content Length 的则通过获取 contentLenght 来计算报文的总长度,长度相等时,返回成功
-            if (contentLengthLines.length == 1) {
-                int contentLength = Integer.parseInt(contentLengthLines[0].split(" ")[1].trim());
-                int totalLength = bufferString.indexOf(BODY_TAG) + 4 + contentLength;
-                if (byteBuffer.limit() >= totalLength) {
-                    return byteBuffer.limit();
+                ByteArrayInputStream bufferStream = new ByteArrayInputStream(byteBuffer.array());
+                String bufferString = new String(TStream.read(bufferStream, bodyTagIndex), "UTF-8");
+
+                //判断是否是 HTTP 头
+                String firstLine = bufferString.substring(0, bufferString.indexOf("\r\n"));
+                if (TString.regexMatch(firstLine, "HTTP\\/\\d\\.\\d\\s\\d{3}\\s.*") <= 0) {
+                    if (TString.regexMatch(firstLine, "^[A-Z]*\\s.*\\sHTTP\\/\\d\\.\\d") <= 0) {
+                        return -1;
+                    }
+                }
+
+                //读取必要参数
+                String[] boundaryLines = TString.searchByRegex(bufferString, "boundary=[^ \\r\\n]+");
+                String[] contentLengthLines = TString.searchByRegex(bufferString, "Content-Length: \\d+");
+                boolean isChunked = bufferString.contains("chunked");
+
+                if (bodyTagIndex != -1) {
+                    // 1.包含 content Length 的则通过获取 contentLenght 来计算报文的总长度,长度相等时,返回成功
+                    if (contentLengthLines.length == 1) {
+                        int contentLength = Integer.parseInt(contentLengthLines[0].split(" ")[1].trim());
+                        int totalLength = bodyTagIndex + 4 + contentLength;
+                        if (byteBuffer.limit() >= totalLength) {
+                            return byteBuffer.limit();
+                        }
+                    }
+
+                    // 2. 如果是 HTTP 响应报文 chunk
+                    // 则trim 后判断最后一个字符是否是 0
+                    if (isChunked) {
+                        bufferStream.skip(bufferStream.available()-7);
+                        byte[] tailBytes = new byte[7];
+                        bufferStream.read(tailBytes);
+                        String tailStr = new String(tailBytes, "UTF-8");
+                        if("\r\n0\r\n\r\n".equals(tailStr) || tailStr.endsWith("\r\n0")) {
+                            return byteBuffer.limit();
+                        }
+                    }
+
+                    // 3.是否是无报文体的简单请求报文(1.Header 中没有 ContentLength / 2.非 Chunked 报文形式)
+                    if (contentLengthLines.length == 0 && !isChunked) {
+                        return byteBuffer.limit();
+                    }
                 }
             }
-
-            // 2. 如果是 HTTP POST报文
-            // POST方法的multipart/form-data类型,且没有指定ContentLength,则需要使用--boundary--的结尾形式来判断
-            if (bufferString.contains("multipart/form-data")
-                    && boundaryLines.length == 1
-                    && bufferString.trim().endsWith("--" + boundaryLines[0].replace("boundary=", "") + "--")) {
-                return byteBuffer.limit();
-            }
-
-            // 3. 如果是 HTTP 响应报文 chunk
-            // 则trim 后判断最后一个字符是否是 0
-            if (isChunked && bufferString.trim().endsWith("\r\n0")) {
-                return byteBuffer.limit();
-            }
-
-            // 4.是否是无报文体的简单请求报文(1.Header 中没有 ContentLength / 2.非 Chunked 报文形式)
-            if (contentLengthLines.length == 0 && !isChunked) {
-                return byteBuffer.limit();
-            }
+	    }catch(Exception e){
+            Logger.error(e);
         }
-
-		return -1;
+        return -1;
 	}
 
 }
