ef2b60cf7b918aa356dd7522317920b6f4640f39
helyho <helyho@gmail.com>
2017-06-12 11:54:11 +0800

    更新 Readme.md

---
+++ All/src/main/java/org/voovan/Voovan.java
@@ -0,0 +1,15 @@
+package org.voovan;
+
+/**
+ * 类文字命名
+ *
+ * @author: helyho
+ * Voovan Framework.
+ * WebSite: https://github.com/helyho/Voovan
+ * Licence: Apache v2 License
+ */
+public class Voovan {
+    private Voovan(){
+
+    }
+}
