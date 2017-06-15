4b75ff46dae5b6e5bd417fa9e34c4457127aa0b4
helyho <helyho@gmail.com>
2017-06-15 19:07:06 +0800

    imp: 优化了 ID 生成器类, 参考 snowflake 算法,修改后的方法

--- Common/src/main/java/org/voovan/tools/TString.java
+++ Common/src/main/java/org/voovan/tools/TString.java
@@ -527,7 +527,7 @@ public class TString {
 		return result;
 	}
 
-	private static String[] chars = new String[] { "0", "1", "2", "3", "4", "5",
+	public static String[] chars = new String[] { "0", "1", "2", "3", "4", "5",
 			"6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
 			"J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
 			"W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i",
