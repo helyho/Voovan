90f5719c2a6fad6b9ac23657f288824b9aed7da2
helyho <helyho@gmail.com>
2017-07-11 20:32:24 +0800

    add: 增加了沙盒对于加载类的安全控制

--- Common/src/test/java/org/voovan/test/tools/compiler/clazz/DynamicClassUnit.java
+++ Common/src/test/java/org/voovan/test/tools/compiler/clazz/DynamicClassUnit.java
@@ -24,13 +24,9 @@ public class DynamicClassUnit extends TestCase{
     private String code;
 
     public void setUp(){
-        SandboxControler sandboxControler = new SandboxControler();
-//        用来测试安全控制
-//        sandboxControler.setExit(false);
-        System.setSecurityManager( new SandboxSecurity(sandboxControler));
         code =  "package org.hocate.test;\r\n\r\n"
                 + "import org.voovan.tools.TString;\r\n"
-                + "public class testSay {\r\n"
+                + "public class TestSay {\r\n"
                 + "\t public String say(){\r\n"
                 + "\t\t System.out.println(\"helloword\");\r\n"
                 + "\t\t return TString.removePrefix(\"finished\"); \r\n"
@@ -43,7 +39,7 @@ public class DynamicClassUnit extends TestCase{
 
     public void testRun() throws Exception{
 //        DynamicClass dynamicClass = new DynamicClass("TestCode",code);  //字符串形式的脚本
-        File codeFile = new File("./src/test/java/org/voovan/test/tools/compiler/clazz/TestClass.vct");
+        File codeFile = new File("./src/test/java/org/voovan/test/tools/compiler/clazz/TestSay.vct");
         DynamicClass dynamicClass = new DynamicClass(codeFile, "UTF-8");   // 文件形式的脚本
 
         for(int i=0;i<4;i++) {
@@ -53,7 +49,7 @@ public class DynamicClassUnit extends TestCase{
             Class clazz = dynamicClass.getClazz();
             System.out.println("==>name:" +dynamicClass.getName());
             System.out.println("==>classname:" +dynamicClass.getClassName());
-            Object testSay = DynamicCompilerManager.newInstance("TestClass",null);
+            Object testSay = DynamicCompilerManager.newInstance("TestSay",null);
             try {
                 Object obj = TReflect.invokeMethod(testSay, "say");
                 System.out.println("==>RunTime: " + (System.currentTimeMillis()-startTime )+"\r\n==>Result: " + obj);
