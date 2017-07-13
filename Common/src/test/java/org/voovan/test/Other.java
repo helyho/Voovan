61382c76dccf064d508257083bd8cb1d465d9ae8
helyho <helyho@gmail.com>
2017-07-13 21:47:57 +0800

    imp: 对于热部署的监控范围控制在 JVM 加载的内容,提高效率 imp: 热部署增加对 jar 包内变更文件的部署 imp: 优化了日志排版,增加了对前景北京颜色的配置支持 add: 增加 TString.split 解决原生类对于两个分割符,无法分割出有效数据的问题

---
+++ Common/src/test/java/org/voovan/test/Other.java
@@ -0,0 +1,27 @@
+package org.voovan.test;
+
+import org.voovan.tools.json.JSON;
+import org.voovan.tools.log.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.regex.Matcher;
+
+/**
+ * 类文字命名
+ *
+ * @author: helyho
+ * Voovan Framework.
+ * WebSite: https://github.com/helyho/Voovan
+ * Licence: Apache v2 License
+ */
+public class Other {
+
+    public static void main(String[] args) throws IOException {
+        Logger.simple("aaaa".split("a").length);
+    }
+
+}
