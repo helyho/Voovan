02f719138319a798df42370abc71921abfc9c271
helyho <helyho@gmail.com>
2017-07-26 18:20:27 +0800

    imp: 初步实现了一个 HashWheelTimer

---
+++ Common/src/main/java/org/voovan/tools/hashwheeltimer/HashWheelTimer.java
@@ -0,0 +1,86 @@
+package org.voovan.tools.hashwheeltimer;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * 类文字命名
+ *
+ * @author: helyho
+ * Voovan Framework.
+ * WebSite: https://github.com/helyho/Voovan
+ * Licence: Apache v2 License
+ */
+public class HashWheelTimer {
+    private HashWheel wheel;
+    private int tickStep = 1;
+    private Timer timer;
+
+    /**
+     * 构造函数
+     *          默认每槽的步长是1s
+     * @param size 时间轮的槽数
+     */
+    public HashWheelTimer(int size){
+        wheel = new HashWheel(size);
+    }
+
+    /**
+     * 构造函数
+     * @param size 时间轮的槽数
+     * @param tickStep 每槽的步长
+     */
+    public HashWheelTimer(int size, int tickStep){
+        wheel = new HashWheel(size);
+        this.tickStep = tickStep;
+    }
+
+    /**
+     * 增加任务
+     * @param task 任务对象
+     * @return true 增加任务成功, false: 增加任务失败, 任务的Interval必须大于0
+     */
+    public boolean addTask(HashWheelTask task, int interval){
+        return wheel.addTask(task, interval);
+    }
+
+    /**
+     * 移除任务
+     * @param tick 槽位号
+     * @param task 任务
+     * @return true:移除任务成功, false:移除任务失败,或任务不存在
+     */
+    public boolean removeTask(int tick, HashWheelTask task){
+        return wheel.removeTask(tick, task);
+    }
+
+
+    /**
+     * 启动时间轮的轮转
+     * @return true:成功启动, false:时间轮已经启动
+     */
+    public boolean rotate(){
+        if(timer != null){
+            return false;
+        }
+
+        final HashWheel rotateWheel = wheel;
+        timer = new Timer();
+        timer.schedule(new TimerTask(){
+
+            @Override
+            public void run() {
+                rotateWheel.run();
+            }
+        }, 0, tickStep*1000);
+
+        return true;
+    }
+
+    /**
+     * 停止时间轮的轮转
+     */
+    public void cancel(){
+        timer.cancel();
+    }
+}
