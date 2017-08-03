4742bc875331ca060aa67b32986a04c8cbb4331e
helyho <helyho@gmail.com>
2017-08-03 11:30:21 +0800

    add: WebServer 增加热部署支持 add: WebServer 增加扫描新类和 jar 到 ClassPath 的支持 fix: TString.fastReplaceAll关于替换字符串的 bug

---
+++ Web/src/main/java/org/voovan/http/server/router/annotation/Router.java
@@ -0,0 +1,28 @@
+package org.voovan.http.server.router.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 请求注解类
+ *
+ * @author: helyho
+ * Voovan Framework.
+ * WebSite: https://github.com/helyho/Voovan
+ * Licence: Apache v2 License
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Router {
+     /**
+      * 请求的 URL
+       * @return
+      */
+    String path() default "/";
+
+     /**
+      * 请求的方法
+      * @return
+      */
+    String method() default "GET";
+}
