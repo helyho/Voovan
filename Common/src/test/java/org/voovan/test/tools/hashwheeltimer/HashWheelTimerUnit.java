afdbe5e57a79680ae198c63377e5e031d746b88a
helyho <helyho@gmail.com>
2017-09-25 04:56:30 +0800

    imp: 修改时间轮定时器,基本单位为1ms

--- Common/src/test/java/org/voovan/test/tools/hashwheeltimer/HashWheelTimerUnit.java
+++ Common/src/test/java/org/voovan/test/tools/hashwheeltimer/HashWheelTimerUnit.java
@@ -23,7 +23,7 @@ public class HashWheelTimerUnit extends TestCase {
     private HashWheelTimer hashWheelTimer;
 
     public void setUp() throws IOException {
-        hashWheelTimer = new HashWheelTimer(5, 1000);
+        hashWheelTimer = new HashWheelTimer(5, 1);
 
         //1个步长
         hashWheelTimer.addTask(new HashWheelTask() {
