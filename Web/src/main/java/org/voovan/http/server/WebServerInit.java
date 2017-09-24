4ea97741d8bbf351de584102ff562abfa3039d2d
helyho <helyho@gmail.com>
2017-09-25 05:04:35 +0800

    add: web.json 的配置文件中增加 InitClass 配置容器在初始化过程中的初始化代码

---
+++ Web/src/main/java/org/voovan/http/server/WebServerInit.java
@@ -0,0 +1,13 @@
+package org.voovan.http.server;
+
+/**
+ * 类文字命名
+ *
+ * @author: helyho
+ * Framework Framework.
+ * WebSite: https://github.com/helyho/Framework
+ * Licence: Apache v2 License
+ */
+public interface WebServerInit {
+    public void init(WebServer webServer);
+}
