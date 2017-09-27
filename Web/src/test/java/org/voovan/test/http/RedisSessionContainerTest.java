ee7a012f26212df4d1981802117f64849a5c299c
helyho <helyho@gmail.com>
2017-09-27 16:29:37 +0800

    imp: 增强RedisMap add: 新增 MemcachedMap, 提供对 Memcached 的支持 add: 新增 Cache 的管理, 增加 Redis 和 MemCached 的默认配置文件加载 imp: 优化 session 的清理工作, 使用 Redis 和 MemCached 时由他们自动清理

--- Web/src/test/java/org/voovan/test/http/RedisSessionContainerTest.java
+++ Web/src/test/java/org/voovan/test/http/RedisSessionContainerTest.java
@@ -1,7 +1,6 @@
-package org.voovan.http.server;
+package org.voovan.test.http;
 
-import org.voovan.tools.RedisMap;
-import redis.clients.jedis.JedisPoolConfig;
+import org.voovan.tools.cache.RedisMap;
 
 /**
  * 基于 Redis 的 Seaaion 共享的样例
@@ -11,9 +10,9 @@ import redis.clients.jedis.JedisPoolConfig;
  * WebSite: https://github.com/helyho/Voovan
  * Licence: Apache v2 License
  */
-public class RedisSessionContainerTest extends RedisSessionContainer{
+public class RedisSessionContainerTest extends RedisMap {
 
     public RedisSessionContainerTest(){
-        super("127.0.0.1", 6379, "123456", 100, 50);
+        super("10.0.0.101", 6379, 2000, 100);
     }
 }
