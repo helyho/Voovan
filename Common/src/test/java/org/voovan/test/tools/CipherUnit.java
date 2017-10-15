3bfa0fd62c4556922480aefe133d0d8c10a09886
helyho <helyho@gmail.com>
2017-10-15 21:33:36 +0800

    fix: 修复对称和非对称加密读取密钥时的 bug

--- Common/src/test/java/org/voovan/test/tools/CipherUnit.java
+++ Common/src/test/java/org/voovan/test/tools/CipherUnit.java
@@ -1,6 +1,7 @@
 package org.voovan.test.tools;
 
 import org.voovan.tools.Cipher;
+import org.voovan.tools.TBase64;
 import org.voovan.tools.log.Logger;
 import junit.framework.TestCase;
 
@@ -9,6 +10,7 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 /**
  * 类文字命名
@@ -20,22 +22,48 @@ import java.security.NoSuchAlgorithmException;
  */
 public class CipherUnit extends TestCase{
 
-    public void testCipher() throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchPaddingException {
+    public void testCipher() throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchPaddingException, InvalidKeySpecException {
         //对称加密
+        Logger.simple("===========生成AES密钥===========");
         Cipher cipher = new Cipher("AES","ECB","PKCS5Padding");
-        cipher.generateSymmetryKey();
-        byte[] msg = cipher.encode("asdfadf".getBytes());
-        Logger.simple(new String(msg));
-        byte[] msg1 = cipher.decode(msg);
-        Logger.simple(new String(msg1));
+        String  ba64Key = TBase64.encode(cipher.generateSymmetryKey());
+        Logger.simple("AES密钥: " +ba64Key);
 
+        Logger.simple("===========读取AES密钥加密===========");
+        cipher = new Cipher("AES","ECB","PKCS5Padding");
+        cipher.loadSymmetryKey(TBase64.decode(ba64Key));
+        byte[] msg = cipher.encode("asdfadf".getBytes());
+        Logger.simple("AES加密后: "+new String(msg));
+
+        Logger.simple("===========读取AES密钥解密===========");
+        cipher = new Cipher("AES","ECB","PKCS5Padding");
+        cipher.loadSymmetryKey(TBase64.decode(ba64Key));
+        byte[] msg1 = cipher.decode(msg);
+        Logger.simple("AES解密后:" + new String(msg1));
+
+        Logger.simple(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 
         //非对称加密
+        Logger.simple("===========生成 RSA 密钥===========");
         cipher = new Cipher("RSA","ECB","PKCS1Padding");
-        cipher.generatPairKey();
+        Cipher.KeyPairStore kps = cipher.generatPairKey();
+        String ba64Private = TBase64.encode(kps.getPrivateKey());
+        String ba64Public = TBase64.encode(kps.getPublicKey());
+        Logger.simple("RSA公钥: " +ba64Private);
+        Logger.simple("RSA私钥: " +ba64Public);
+
+
+        Logger.simple("===========RSA读取公钥加密===========");
+        cipher = new Cipher("RSA","ECB","PKCS1Padding");
+        cipher.loadPublicKey(TBase64.decode(ba64Public));
         msg = cipher.encode("asdfadf".getBytes());
-        Logger.simple(new String(msg));
+        Logger.simple(" RSA加密后: "+new String(msg));
+
+
+        Logger.simple("===========RSA读取私钥解密===========");
+        cipher = new Cipher("RSA","ECB","PKCS1Padding");
+        cipher.loadPrivateKey(TBase64.decode(ba64Private));
         msg1 = cipher.decode(msg);
-        Logger.simple(new String(msg1));
+        Logger.simple("RSA解密后: "+new String(msg1));
     }
 }
