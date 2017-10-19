af8654e84c89e6eb1fa3475a71fa82f032e0fe52
helyho <helyho@gmail.com>
2017-10-20 00:14:52 +0800

    fix: 修复 send 在并发场景下可能出现的数据交叉发送的问题

--- Network/src/main/java/org/voovan/network/aio/AioSession.java
+++ Network/src/main/java/org/voovan/network/aio/AioSession.java
@@ -15,170 +15,170 @@ import java.util.concurrent.Future;
 
 /**
  * NIO 会话连接对象
- * 
+ *
  * @author helyho
  *
  * Voovan Framework.
  * WebSite: https://github.com/helyho/Voovan
  * Licence: Apache v2 License
  */
-public class AioSession extends IoSession<AioSocket>  {
+public class AioSession extends IoSession<AioSocket> {
 
-	private AsynchronousSocketChannel	socketChannel;
+    private AsynchronousSocketChannel	socketChannel;
 
 
-	/**
-	 * 构造函数
-	 * 
-	 * @param socket AioSocket对象
-	 */
-	public AioSession(AioSocket socket) {
-		super(socket);
-		if (socket != null) {
-			this.socketChannel = socket.socketChannel();
-		} else {
-			Logger.error("SocketChannel is null, please check it.");
-		}
-	}
+    /**
+     * 构造函数
+     *
+     * @param socket AioSocket对象
+     */
+    public AioSession(AioSocket socket) {
+        super(socket);
+        if (socket != null) {
+            this.socketChannel = socket.socketChannel();
+        } else {
+            Logger.error("SocketChannel is null, please check it.");
+        }
+    }
 
-	@Override
-	public String localAddress() {
-		if (this.isConnected()) {
-			try {
-				InetSocketAddress socketAddress = (InetSocketAddress)socketChannel.getLocalAddress();
-				return socketAddress.getHostName();
-			} catch (IOException e) {
-				Logger.error("Get SocketChannel local address failed",e);
-				return null;
-			}
-		} else {
-			return null;
-		}
-	}
+    @Override
+    public String localAddress() {
+        if (this.isConnected()) {
+            try {
+                InetSocketAddress socketAddress = (InetSocketAddress)socketChannel.getLocalAddress();
+                return socketAddress.getHostName();
+            } catch (IOException e) {
+                Logger.error("Get SocketChannel local address failed",e);
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
 
-	@Override
-	public int loaclPort() {
-		if (this.isConnected()) {
-			try {
-				InetSocketAddress socketAddress = (InetSocketAddress)socketChannel.getLocalAddress();
-				return socketAddress.getPort();
-			} catch (IOException e) {
-				Logger.error("Get SocketChannel local port failed",e);
-				return -1;
-			}
-		} else {
-			return -1;
-		}
-	}
+    @Override
+    public int loaclPort() {
+        if (this.isConnected()) {
+            try {
+                InetSocketAddress socketAddress = (InetSocketAddress)socketChannel.getLocalAddress();
+                return socketAddress.getPort();
+            } catch (IOException e) {
+                Logger.error("Get SocketChannel local port failed",e);
+                return -1;
+            }
+        } else {
+            return -1;
+        }
+    }
 
-	@Override
-	public String remoteAddress() {
-		if (this.isConnected()) {
-			try {
-				InetSocketAddress socketAddress = (InetSocketAddress)socketChannel.getRemoteAddress();
-				return socketAddress.getHostString();
-			} catch (IOException e) {
-				Logger.error("Get SocketChannel remote address failed",e);
-				return null;
-			}
-		} else {
-			return null;
-		}
-	}
+    @Override
+    public String remoteAddress() {
+        if (this.isConnected()) {
+            try {
+                InetSocketAddress socketAddress = (InetSocketAddress)socketChannel.getRemoteAddress();
+                return socketAddress.getHostString();
+            } catch (IOException e) {
+                Logger.error("Get SocketChannel remote address failed",e);
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
 
-	@Override
-	public int remotePort() {
-		if (this.isConnected()) {
-			try {
-				InetSocketAddress socketAddress = (InetSocketAddress)socketChannel.getRemoteAddress();
-				return socketAddress.getPort();
-			} catch (IOException e) {
-				Logger.error("Get SocketChannel remote port failed.",e);
-				return -1;
-			}
-		} else {
-			return -1;
-		}
-	}
+    @Override
+    public int remotePort() {
+        if (this.isConnected()) {
+            try {
+                InetSocketAddress socketAddress = (InetSocketAddress)socketChannel.getRemoteAddress();
+                return socketAddress.getPort();
+            } catch (IOException e) {
+                Logger.error("Get SocketChannel remote port failed.",e);
+                return -1;
+            }
+        } else {
+            return -1;
+        }
+    }
 
-	@Override
-	protected int read0(ByteBuffer buffer) throws IOException {
-		int readSize = 0;
-		if (buffer != null && !this.getByteBufferChannel().isReleased()) {
-			readSize = this.getByteBufferChannel().readHead(buffer);
-		}
-		return readSize;
-	}
+    @Override
+    protected int read0(ByteBuffer buffer) throws IOException {
+        int readSize = 0;
+        if (buffer != null && !this.getByteBufferChannel().isReleased()) {
+            readSize = this.getByteBufferChannel().readHead(buffer);
+        }
+        return readSize;
+    }
 
-	@Override
-	protected int send0(ByteBuffer buffer) throws IOException {
-		int totalSendByte = 0;
-		if (isConnected() && buffer != null) {
-			//循环发送直到全部内容发送完毕
-			while(isConnected() && buffer.remaining()!=0){
-				try {
-					Future<Integer> sendResult = socketChannel.write(buffer);
-					if(sendResult==null){
-						break;
-					}
+    @Override
+    protected synchronized int send0(ByteBuffer buffer) throws IOException {
+        int totalSendByte = 0;
+        if (isConnected() && buffer != null) {
+            //循环发送直到全部内容发送完毕
+            while(isConnected() && buffer.remaining()!=0){
+                try {
+                    Future<Integer> sendResult = socketChannel.write(buffer);
+                    if(sendResult==null){
+                        break;
+                    }
 
-					try {
-						totalSendByte += sendResult.get();
-					} catch (InterruptedException | ExecutionException e) {
-						close();
-					}
+                    try {
+                        totalSendByte += sendResult.get();
+                    } catch (InterruptedException | ExecutionException e) {
+                        close();
+                    }
 
-				}catch(WritePendingException e){
-					continue;
-				}
-			}
-		}
-		return totalSendByte;
-	}
+                }catch(WritePendingException e){
+                    continue;
+                }
+            }
+        }
+        return totalSendByte;
+    }
 
-	@Override
-	protected MessageSplitter getMessagePartition() {
-		return this.socketContext().messageSplitter();
-	}
+    @Override
+    protected MessageSplitter getMessagePartition() {
+        return this.socketContext().messageSplitter();
+    }
 
-	@Override
-	public boolean isConnected() {
-		return this.socketContext().isConnected();
-	}
+    @Override
+    public boolean isConnected() {
+        return this.socketContext().isConnected();
+    }
 
-	/**
-	 * 会话是否打开
-	 *
-	 * @return true: 打开,false: 关闭
-	 */
-	@Override
-	public boolean isOpen() {
-		return this.socketContext().isOpen();
-	}
+    /**
+     * 会话是否打开
+     *
+     * @return true: 打开,false: 关闭
+     */
+    @Override
+    public boolean isOpen() {
+        return this.socketContext().isOpen();
+    }
 
-	@Override
-	public boolean close() {
-		this.cancelIdle();
+    @Override
+    public boolean close() {
+        this.cancelIdle();
 
-		// 关闭 socket
-		if(isConnected()) {
-			return this.socketContext().close();
-		}else{
-			return false;
-		}
-	}
+        // 关闭 socket
+        if(isConnected()) {
+            return this.socketContext().close();
+        }else{
+            return false;
+        }
+    }
 
-	/**
-	 * 重连当前连接
-	 * @throws IOException IO 异常
-	 * @throws RestartException 重新启动的异常
-	 */
-	public void restart() throws IOException, RestartException {
-		socketContext().restart();
-	}
+    /**
+     * 重连当前连接
+     * @throws IOException IO 异常
+     * @throws RestartException 重新启动的异常
+     */
+    public void restart() throws IOException, RestartException {
+        socketContext().restart();
+    }
 
-	@Override
-	public String toString() {
-		return "[" + this.localAddress() + ":" + this.loaclPort() + "] -> [" + this.remoteAddress() + ":" + this.remotePort() + "]";
-	}
+    @Override
+    public String toString() {
+        return "[" + this.localAddress() + ":" + this.loaclPort() + "] -> [" + this.remoteAddress() + ":" + this.remotePort() + "]";
+    }
 }
