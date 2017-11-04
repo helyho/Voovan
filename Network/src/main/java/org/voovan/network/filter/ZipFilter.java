56830bf29a1343c652e12238df58a3e06f5343c0
helyho <helyho@gmail.com>
2017-11-05 01:46:02 +0800

    add: 新增 ZipFilter 降低数据传输的大小 fix: 修复 send 超时的问题

---
+++ Network/src/main/java/org/voovan/network/filter/ZipFilter.java
@@ -0,0 +1,41 @@
+package org.voovan.network.filter;
+
+import org.voovan.network.IoFilter;
+import org.voovan.network.IoSession;
+import org.voovan.network.exception.IoFilterException;
+import org.voovan.tools.TZip;
+
+import java.io.IOException;
+
+/**
+ * Some description
+ *
+ * @author: helyho
+ * Project: DBase
+ * Create: 2017/11/3 21:08
+ */
+public class ZipFilter implements IoFilter{
+    @Override
+    public Object decode(IoSession session, Object object) throws IoFilterException {
+        if(object.getClass() == ByteFilter.BYTE_ARRAY_CLASS){
+            try {
+                return TZip.decodeGZip((byte[])object);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Object encode(IoSession session, Object object) throws IoFilterException {
+        if(object.getClass() == ByteFilter.BYTE_ARRAY_CLASS){
+            try {
+                return TZip.encodeGZip((byte[])object);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
