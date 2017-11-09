a7c48893a47c2f833f5c60c2b8056d02674a0ac5
helyho <helyho@gmail.com>
2017-11-09 19:22:23 +0800

    fix: 修复 HttpMessageSplitter 对于非Http请求报文不关闭连接的问题

--- Network/src/main/java/org/voovan/network/messagesplitter/HttpMessageSplitter.java
+++ Network/src/main/java/org/voovan/network/messagesplitter/HttpMessageSplitter.java
@@ -3,7 +3,6 @@ package org.voovan.network.messagesplitter;
 import org.voovan.network.IoSession;
 import org.voovan.network.MessageSplitter;
 import org.voovan.tools.TByteBuffer;
-import org.voovan.tools.TString;
 
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
@@ -29,6 +28,7 @@ public class HttpMessageSplitter implements MessageSplitter {
 
     @Override
     public int canSplite(IoSession session, ByteBuffer byteBuffer) {
+        //返回 -1: 报文未完整接受, -2: 报文异常, 管理连接
 
         if(byteBuffer.limit()==0){
             return -1;
@@ -42,17 +42,17 @@ public class HttpMessageSplitter implements MessageSplitter {
             if(result>0){
                 result = 0;
             }else if(result == -1){
+                return result;
+            }else if(result == -2){
                 session.close();
             }
         }
-
-
 
         return result;
     }
 
     private int isHttpFrame(ByteBuffer byteBuffer){
-        int bodyTagIndex = 0;
+        int bodyTagIndex = -1;
         byte[] buffer = TByteBuffer.toArray(byteBuffer);
         StringBuilder stringBuilder = new StringBuilder();
         String httpHead = null;
@@ -60,12 +60,13 @@ public class HttpMessageSplitter implements MessageSplitter {
         for(int x=0;x<buffer.length-3;x++){
             if(buffer[x] == '\r' && buffer[x+1] == '\n' && buffer[x+2] == '\r' && buffer[x+3] == '\n'){
                 bodyTagIndex = x + 3;
-                httpHead = stringBuilder.toString();
                 break;
             }else{
                 stringBuilder.append((char)buffer[x]);
             }
         }
+
+        httpHead = stringBuilder.toString();
 
         if(httpHead !=null && isHttpHead(httpHead)) {
 
@@ -84,7 +85,11 @@ public class HttpMessageSplitter implements MessageSplitter {
 //            }
 //
 //            isChunked = httpHead.contains("chunked");
-            return bodyTagIndex;
+            if(bodyTagIndex > 0) {
+                return bodyTagIndex;
+            } else {
+                return -1;
+            }
 
         }else{
             return -2;
@@ -114,13 +119,13 @@ public class HttpMessageSplitter implements MessageSplitter {
         // 期望数据包的实际大小
         int expectPackagesize = 2;
         if (maxpacketsize < expectPackagesize) {
-            return -1;
+            return -2;
         }
         byte finByte = buffer.get();
         boolean fin = finByte >> 8 != 0;
         byte rsv = (byte) ((finByte & ~(byte) 128) >> 4);
         if (rsv != 0) {
-            return -1;
+            return -2;
         }
         byte maskByte = buffer.get();
         boolean mask = (maskByte & -128) != 0;
@@ -128,19 +133,19 @@ public class HttpMessageSplitter implements MessageSplitter {
         int optcode = (byte) (finByte & 15);
 
         if(optcode < 0 && optcode > 10){
-            return -1;
+            return -2;
         }
 
         if (!fin) {
             if (optcode == 9 || optcode == 10 || optcode == 8) {
-                return -1;
+                return -2;
             }
         }
 
         if (payloadlength >= 0 && payloadlength <= 125) {
         } else {
             if (optcode == 9 || optcode == 10 || optcode == 8) {
-                return -1;
+                return -2;
             }
             if (payloadlength == 126) {
                 expectPackagesize += 2;
@@ -167,7 +172,7 @@ public class HttpMessageSplitter implements MessageSplitter {
         // 如果实际接受的数据小于数据包的大小则报错
         if (maxpacketsize < expectPackagesize) {
             buffer.position(0);
-            return -1;
+            return -2;
         } else {
             buffer.position(0);
             return expectPackagesize;
