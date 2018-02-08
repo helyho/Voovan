a86aa5a3125bc4772866f6df4b3124c04312a2b7
helyho <helyho@gmail.com>
2018-02-08 22:15:16 +0800

    add: 为 Recorder 增加 @NotInsert 注解

---
+++ Database/src/main/java/org/voovan/db/recorder/annotation/NotInsert.java
@@ -0,0 +1,12 @@
+package org.voovan.db.recorder.annotation;
+
+/**
+ * 类文字命名
+ *
+ * @author: helyho
+ * DBase Framework.
+ * WebSite: https://github.com/helyho/DBase
+ * Licence: Apache v2 License
+ */
+public @interface NotInsert {
+}
