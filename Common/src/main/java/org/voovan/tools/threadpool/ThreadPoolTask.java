c716df1749ff1c81af29844c27623e364a3741b9
helyho <helyho@gmail.com>
2018-05-02 19:36:48 +0800

    fix: 修复线程监视任务失败的问题

--- Common/src/main/java/org/voovan/tools/threadpool/ThreadPoolTask.java
+++ Common/src/main/java/org/voovan/tools/threadpool/ThreadPoolTask.java
@@ -27,54 +27,56 @@ public class ThreadPoolTask extends TimerTask {
 
 	@Override
 	public void run() {
-		if (threadPoolInstance.isShutdown()) {
-			this.cancel();
-			System.exit(0);
-		}
-
-		String threadPoolInfo = "[PoolInfo]: " + threadPoolInstance.getActiveCount() + "/" + threadPoolInstance.getCorePoolSize() + "/"
-				+ threadPoolInstance.getLargestPoolSize()+ "/" + threadPoolInstance.getMaximumPoolSize() + " \t[TaskCount]: "
-				+ threadPoolInstance.getCompletedTaskCount() + "/"
-				+ threadPoolInstance.getTaskCount() + " \t[QueueSize]: " + threadPoolInstance.getQueue().size() + " \t[PerCoreLoadAvg]: "
-				+ TPerformance.cpuPerCoreLoadAvg();
-		if (threadPoolInstance.getActiveCount() != 0) {
-			Logger.fremawork("[ShutDown]: " + threadPoolInstance.isShutdown() + " \t" + threadPoolInfo);
-		}
-
-		int poolSize = threadPoolInstance.getPoolSize();
-		// 动态调整线程数,且系统CPU负载值要小于1
-		if (threadPoolInstance.getQueue().size() > 0 ) {
-
-			int newPoolSize = (int)(threadPoolInstance.getCorePoolSize() * 1.20);
-
-			if(newPoolSize > ThreadPool.MAX_POOL_SIZE){
-				newPoolSize = ThreadPool.MAX_POOL_SIZE;
+		try {
+			if (threadPoolInstance.isShutdown()) {
+				this.cancel();
+				System.exit(0);
 			}
 
-			if(newPoolSize!=poolSize) {
-				threadPoolInstance.setCorePoolSize(newPoolSize);
-				Logger.fremawork("PoolSizeChange: " + poolSize + "->" + threadPoolInstance.getCorePoolSize());
-			}
-		}
-
-		else if(threadPoolInstance.getActiveCount() <= threadPoolInstance.getPoolSize()/2 &&
-				threadPoolInstance.getCorePoolSize() > ThreadPool.MIN_POOL_SIZE){
-
-			int newPoolsize = (int)(threadPoolInstance.getCorePoolSize()*0.8);
-
-			if(newPoolsize < ThreadPool.MIN_POOL_SIZE){
-				newPoolsize = ThreadPool.MIN_POOL_SIZE;
+			String threadPoolInfo = "[PoolInfo]: " + threadPoolInstance.getActiveCount() + "/" + threadPoolInstance.getCorePoolSize() + "/"
+					+ threadPoolInstance.getLargestPoolSize() + "/" + threadPoolInstance.getMaximumPoolSize() + " \t[TaskCount]: "
+					+ threadPoolInstance.getCompletedTaskCount() + "/"
+					+ threadPoolInstance.getTaskCount() + " \t[QueueSize]: " + threadPoolInstance.getQueue().size() + " \t[PerCoreLoadAvg]: "
+					+ TPerformance.cpuPerCoreLoadAvg();
+			if (threadPoolInstance.getActiveCount() != 0) {
+				Logger.fremawork("[ShutDown]: " + threadPoolInstance.isShutdown() + " \t" + threadPoolInfo);
 			}
 
-			if(newPoolsize != poolSize) {
-				threadPoolInstance.setCorePoolSize(newPoolsize);
-				Logger.fremawork("PoolSizeChange: " + poolSize + "->" + threadPoolInstance.getCorePoolSize());
-			}
-		}
+			int poolSize = threadPoolInstance.getPoolSize();
+			// 动态调整线程数,且系统CPU负载值要小于1
+			if (threadPoolInstance.getQueue().size() > 0) {
 
-		//如果主线程结束,则线程池也关闭
-		if( TEnv.isMainThreadShutDown()) {
-			threadPoolInstance.shutdown();
+				int newPoolSize = (int) (threadPoolInstance.getCorePoolSize() * 1.20);
+
+				if (newPoolSize > ThreadPool.MAX_POOL_SIZE) {
+					newPoolSize = ThreadPool.MAX_POOL_SIZE;
+				}
+
+				if (newPoolSize != poolSize) {
+					threadPoolInstance.setCorePoolSize(newPoolSize);
+					Logger.fremawork("PoolSizeChange: " + poolSize + "->" + threadPoolInstance.getCorePoolSize());
+				}
+			} else if (threadPoolInstance.getActiveCount() <= threadPoolInstance.getPoolSize() / 2 &&
+					threadPoolInstance.getCorePoolSize() > ThreadPool.MIN_POOL_SIZE) {
+
+				int newPoolsize = (int) (threadPoolInstance.getCorePoolSize() * 0.8);
+
+				if (newPoolsize < ThreadPool.MIN_POOL_SIZE) {
+					newPoolsize = ThreadPool.MIN_POOL_SIZE;
+				}
+
+				if (newPoolsize != poolSize) {
+					threadPoolInstance.setCorePoolSize(newPoolsize);
+					Logger.fremawork("PoolSizeChange: " + poolSize + "->" + threadPoolInstance.getCorePoolSize());
+				}
+			}
+
+			//如果主线程结束,则线程池也关闭
+			if (TEnv.isMainThreadShutDown()) {
+				threadPoolInstance.shutdown();
+			}
+		} catch (Exception e){
+			Logger.error("Threadpooltask error: ", e);
 		}
 	}
 }
