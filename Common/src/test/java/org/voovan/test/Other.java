4cdc670cd98b1fc98d879152c20db36457d56001
helyho <helyho@gmail.com>
2018-05-30 14:52:46 +0800

    imp: 优化 内存管理方式 异步待完善, 还剩下打开合并内存块导致的问题

--- Common/src/test/java/org/voovan/test/Other.java
+++ Common/src/test/java/org/voovan/test/Other.java
@@ -1,11 +1,13 @@
 package org.voovan.test;
 
+import org.voovan.Global;
 import org.voovan.tools.Memory;
 import org.voovan.tools.TEnv;
 import org.voovan.tools.TString;
 import org.voovan.tools.log.Logger;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 
 /**
@@ -19,28 +21,34 @@ import java.util.regex.Matcher;
 public class Other {
 
     public static void main(String[] args) throws IOException {
-        Memory memory = new Memory(1024*1024*1024*2L);
+        Memory memory = new Memory(1024*1024);
         Logger.info("start");
         System.out.println(memory);
 
-        long address = memory.allocate(1024*1024);
-        System.out.println(address);
+        long address1 = memory.allocate(128);
+        System.out.println(address1);
 
         TEnv.sleep(10);
 
+        AtomicInteger in = new AtomicInteger(0);
         for(int i=0;i<100;i++) {
-            address = memory.allocate(1024 * 1024);
-            if(i%10<9){
+            int x = i;
+            Global.getThreadPool().execute(()->{
+                long address = memory.allocate((long) (1024));
+                TEnv.sleep((int) (Math.random()*5));
+                if(x%10 <5)
                 memory.release(address);
-            }
-            System.out.println(address);
+                in.incrementAndGet();
+                System.out.println(address);
+            });
         }
 
 
         Logger.info("end");
 
         while (true){
-            TEnv.sleep(1);
+            TEnv.sleep(1000);
+            System.out.println(in.get());
         }
     }
 }
