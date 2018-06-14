44bd341c19f7d435cabf9f0f3014c16d2fb7dfd5
helyho <helyho@gmail.com>
2018-06-14 22:22:15 +0800

    imp: Socket 性能优化

--- Network/src/main/java/org/voovan/network/EventProcess.java
+++ Network/src/main/java/org/voovan/network/EventProcess.java
@@ -251,41 +251,41 @@ public class EventProcess {
 		final Object sendObj = obj;
 
 		//开启一个线程发送消息,不阻塞当前线程
+        try {
+            sendSession.getState().sendLock();
             try {
-                sendSession.getState().sendLock();
+                int sendCount = -1;
+
                 try {
-                    int sendCount = -1;
+                    // ------------------Filter 加密处理-----------------
+                    ByteBuffer sendBuffer = EventProcess.filterEncoder(sendSession, sendObj);
+                    // ---------------------------------------------------
 
-                    try {
-                        // ------------------Filter 加密处理-----------------
-                        ByteBuffer sendBuffer = EventProcess.filterEncoder(sendSession, sendObj);
-                        // ---------------------------------------------------
+                    if (sendBuffer != null) {
 
-                        if (sendBuffer != null) {
-
-                            // 发送消息
-                            if (sendBuffer != null && sendSession.isOpen()) {
-                                if (sendBuffer.limit() > 0) {
-                                    sendCount = sendSession.send(sendBuffer);
-                                    sendBuffer.rewind();
-                                }
+                        // 发送消息
+                        if (sendBuffer != null && sendSession.isOpen()) {
+                            if (sendBuffer.limit() > 0) {
+                                sendCount = sendSession.send(sendBuffer);
+                                sendBuffer.rewind();
                             }
-
-                            //触发发送事件
-                            EventTrigger.fireSent(sendSession, sendObj);
                         }
-                    } catch (IoFilterException e) {
-                        EventTrigger.fireException(sendSession, e);
+
+                        //触发发送事件
+                        EventTrigger.fireSent(sendSession, sendObj);
                     }
-
-
-                } finally {
-                    sendSession.getState().sendUnLock();
-                    sendSession.getState().setSend(false);
+                } catch (IoFilterException e) {
+                    EventTrigger.fireException(sendSession, e);
                 }
-            } catch (InterruptedException e) {
-                EventTrigger.fireException(sendSession, e);
+
+
+            } finally {
+                sendSession.getState().sendUnLock();
+                sendSession.getState().setSend(false);
             }
+        } catch (InterruptedException e) {
+            EventTrigger.fireException(sendSession, e);
+        }
 	}
 
 	/**
