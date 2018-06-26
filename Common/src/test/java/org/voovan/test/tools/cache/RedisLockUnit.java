7a86ba5edf143531b4cb0a7869b3751442c60ec0
helyho <helyho@gmail.com>
2018-06-26 18:46:59 +0800

    fix: 分布式锁 bug 修复

--- Common/src/test/java/org/voovan/test/tools/cache/RedisLockUnit.java
+++ Common/src/test/java/org/voovan/test/tools/cache/RedisLockUnit.java
@@ -1,12 +1,13 @@
 package org.voovan.test.tools.cache;
 
-import junit.framework.TestCase;
 import org.voovan.Global;
 import org.voovan.tools.TEnv;
 import org.voovan.tools.cache.RedisLock;
 import org.voovan.tools.cache.RedisMap;
+import junit.framework.TestCase;
 
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 /**
  * 类文字命名
@@ -19,6 +20,8 @@ import java.util.ArrayList;
 public class RedisLockUnit extends TestCase{
 
     private RedisLock redisLock;
+
+    Semaphore semaphore = new Semaphore(1);
 
     public volatile static int x = 0;
 
@@ -40,13 +43,21 @@ public class RedisLockUnit extends TestCase{
             Global.getThreadPool().execute(new Runnable() {
                 @Override
                 public void run() {
-                    redisLock.lock(1000*60, 500);
-                    System.out.println(Long.toString(System.nanoTime())+ "\tlock");
+                    while(!redisLock.lock(1000*60, 5000)){
+                        TEnv.sleep(1);
+                    }
+                    boolean locked = semaphore.tryAcquire();
+                    System.out.println(Long.toString(System.nanoTime())+ "\tlock " + x + " " + locked);
                     int lock = (int)Math.random()*300;
                     TEnv.sleep(lock);
                     x++;
-                    System.out.println(Long.toString(System.nanoTime())+ "\tunlock");
-                    redisLock.unLock();
+                    if(locked) {
+                        semaphore.release();
+                    }
+                    System.out.println(Long.toString(System.nanoTime())+ "\tunlock " + x);
+                    while(!redisLock.unLock()){
+                        TEnv.sleep(1);
+                    }
 
                 }
             });
