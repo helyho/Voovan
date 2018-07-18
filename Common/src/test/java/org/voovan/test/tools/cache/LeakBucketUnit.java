92f26dd09655d5e74df872ae9c0c8b2a770d84b2
helyho <helyho@gmail.com>
2018-07-19 03:31:29 +0800

    add: 新增无锁的令牌桶和漏桶

---
+++ Common/src/test/java/org/voovan/test/tools/cache/LeakBucketUnit.java
@@ -0,0 +1,72 @@
+package org.voovan.test.tools.cache;
+
+import org.voovan.Global;
+import org.voovan.tools.TEnv;
+import org.voovan.tools.cache.LeakBucket;
+import junit.framework.TestCase;
+
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 类文字命名
+ *
+ * @author: helyho
+ * DBase Framework.
+ * WebSite: https://github.com/helyho/DBase
+ * Licence: Apache v2 License
+ */
+public class LeakBucketUnit extends TestCase {
+
+    public void testNormal(){
+        LeakBucket tokenBucket = new LeakBucket(2, 1000);
+
+        System.out.println(tokenBucket.acquire());
+        System.out.println(tokenBucket.acquire());
+        System.out.println(tokenBucket.acquire());
+        TEnv.sleep(1000);
+        System.out.println(tokenBucket.acquire());
+        System.out.println(tokenBucket.acquire());
+        System.out.println(tokenBucket.acquire());
+
+        TEnv.sleep(3000);
+
+        System.out.println("================================================");
+
+        try {
+            tokenBucket.acquire(1000);
+            System.out.println(true);
+        } catch (TimeoutException e) {
+            System.out.println(false);
+        }
+        try {
+            tokenBucket.acquire(500);
+            System.out.println(true);
+        } catch (TimeoutException e) {
+            System.out.println(false);
+        }
+
+        try {
+            tokenBucket.acquire(500);
+            System.out.println(true);
+        } catch (TimeoutException e) {
+            System.out.println(false);
+        }
+    }
+
+    public void testParaller(){
+        final LeakBucket leakBucket = new LeakBucket(2, 1000);
+        for(int i=0; i <20; i++) {
+            Global.getThreadPool().execute(() -> {
+                TEnv.sleep((int) (Math.random() * 1000));
+                try {
+                    leakBucket.acquire(1000);
+                    System.out.println(true);
+                } catch (TimeoutException e) {
+                    System.out.println(false);
+                }
+            });
+        }
+
+        TEnv.sleep(1000*10);
+    }
+}
