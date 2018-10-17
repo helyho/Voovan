b19f1fa12eaf543037236bb593507712fe36aab1
helyho <helyho@gmail.com>
2018-10-17 17:32:45 +0800

    imp: TReflect.systemPackages 新增 javassist

--- Common/src/main/java/org/voovan/tools/reflect/TReflect.java
+++ Common/src/main/java/org/voovan/tools/reflect/TReflect.java
@@ -1202,7 +1202,7 @@ public class TReflect {
 		}
 	}
 
-	private static List<String> systemPackages = TObject.asList("java.","jdk.","sun.","javax.","com.sun","com.oracle");
+	private static List<String> systemPackages = TObject.asList("java.","jdk.","sun.","javax.","com.sun","com.oracle","javassist");
 
 	/**
 	 * 判读是否是 JDK 中定义的类(java包下的所有类)
