8fc8454ce36aff6d7b27c2b430e629674d20649a
helyho <helyho@gmail.com>
2018-11-05 13:10:13 +0800

    add: JSON 新增是否格式化特殊字符串的系统配置

--- Common/src/main/java/org/voovan/tools/json/JSON.java
+++ Common/src/main/java/org/voovan/tools/json/JSON.java
@@ -1,5 +1,6 @@
 package org.voovan.tools.json;
 
+import org.voovan.tools.TProperties;
 import org.voovan.tools.TString;
 import org.voovan.tools.log.Logger;
 
@@ -17,14 +18,23 @@ import java.util.function.Supplier;
  * Licence: Apache v2 License
  */
 public class JSON {
-
+	public final static String JSON_CONVERT_ESCAPE_CHAR = TProperties.getString("framework", "JSONConvertEscapeChar");
 	/**
-	 * 是否进行 EscapeChar 的转换, 默认 true, 当你确定你不存在字符串多行换行的时候可设置为 true
+	 * 是否进行 EscapeChar 的转换, 默认 true, 当你确定你不存在字符串多行换行的时候可设置为 false
 	 */
 	private static ThreadLocal<Boolean> convertEscapeChar = ThreadLocal.withInitial(new Supplier<Boolean>() {
 		@Override
 		public Boolean get() {
-			return true;
+
+			Logger.simple("[SYSTEM] JSON convert escape_char: " + (JSON_CONVERT_ESCAPE_CHAR==null?"true":"false"));
+
+			if("true".equalsIgnoreCase(JSON_CONVERT_ESCAPE_CHAR)) {
+				return true;
+			} if("false".equalsIgnoreCase(JSON_CONVERT_ESCAPE_CHAR)) {
+				return false;
+			} else {
+				return true;
+			}
 		}
 	});
 
