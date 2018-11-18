7c4ea6fb36633363be511bf1bdb800141a637ebd
helyho <helyho@gmail.com>
2018-11-18 14:38:18 +0800

    imp: Aop测试类完善

--- Common/src/test/java/org/voovan/test/tools/aop/CutPointUtil.java
+++ Common/src/test/java/org/voovan/test/tools/aop/CutPointUtil.java
@@ -1,12 +1,10 @@
 package org.voovan.test.tools.aop;
 
-import junit.framework.TestCase;
 import org.voovan.tools.aop.InterceptInfo;
-import org.voovan.tools.aop.annotation.After;
-import org.voovan.tools.aop.annotation.Aop;
-import org.voovan.tools.aop.annotation.Around;
-import org.voovan.tools.aop.annotation.Before;
+import org.voovan.tools.aop.annotation.*;
+import org.voovan.tools.aop.annotation.Abnormal;
 import org.voovan.tools.json.JSON;
+import org.voovan.tools.log.Logger;
 
 /**
  * 类文字命名
@@ -17,16 +15,17 @@ import org.voovan.tools.json.JSON;
  * Licence: Apache v2 License
  */
 @Aop
-public class CutPointUtil extends TestCase{
+public class CutPointUtil {
+
 
     public String testBefore(int mm){
-        System.out.println("testMethod2--->" + mm);
+        Logger.simple("testMethod2--->" + mm);
         return "ffff";
     }
 
 
     public int testAfter(String mm){
-        System.out.println("testMethod--->" + mm);
+        Logger.simple("testMethod--->" + mm);
         return 234;
     }
 
@@ -38,8 +37,8 @@ public class CutPointUtil extends TestCase{
         }
     }
 
-    public String testAround(int mm){
-        System.out.println("around--->" + mm);
+    public static String testAround(int mm){
+        Logger.simple("around--->" + mm);
         return "ffff" + mm;
     }
 
@@ -49,37 +48,49 @@ public class CutPointUtil extends TestCase{
 
     @Before("* org.voovan.test.tools.aop.CutPointUtil@testBefore(..)")
     public static void cutPointBefore(InterceptInfo interceptInfo){
-        System.out.println("before========>");
+        Logger.simple("before========>");
     }
 
     @After("* org.voovan.test.*.*.CutPointUtil@testAfter(java.lang.String)")
     public static String cutPointAfter(InterceptInfo interceptInfo){
-        System.out.println("after========>" + JSON.toJSON(interceptInfo));
+        Logger.simple("after========>" + JSON.toJSON(interceptInfo));
         return "-----////";
     }
 
     @Around("* org.voovan.test.*.*.CutPointUtil@testAround(int)")
     public static String cutPointAround(InterceptInfo interceptInfo) throws Throwable {
-        System.out.println("after========>" + JSON.toJSON(interceptInfo));
+        Logger.simple("Around========>" + JSON.toJSON(interceptInfo));
         Object result = interceptInfo.process();
         return "-----////" + result;
     }
 
-    @org.voovan.tools.aop.annotation.Abnormal("* org.voovan.test.*.*.CutPointUtil@testException(int)")
+    @Abnormal("* org.voovan.test.*.*.CutPointUtil@testException(int)")
     public static String cutPointCatch(InterceptInfo interceptInfo){
-        System.out.println("after========>" + JSON.toJSON(interceptInfo));
+        Logger.simple("Abnormal========>" + JSON.toJSON(interceptInfo));
         return "-----////";
     }
 
     @After("void org.voovan.test.*.*.CutPointUtil@mmm()")
     public static String testmmmm(InterceptInfo interceptInfo){
-        System.out.println("after========>" + JSON.toJSON(interceptInfo));
+        Logger.simple("after========>" + JSON.toJSON(interceptInfo));
         return "-----////";
     }
 
-    @After("* org.voovan.test.http.router.AnnotationRouterTest@index()")
+    @After("* org.voovan.test.service.web.IndexMethod@index(..)")
     public static String indexMethodAfter(InterceptInfo interceptInfo){
-        System.out.println("after========>" + JSON.toJSON(interceptInfo));
+        Logger.simple("after========>" + JSON.toJSON(interceptInfo));
         return "-----////";
+    }
+
+    @Around("* org.voovan.test.service.web.IndexMethod@index(..)")
+    public static String indexMethodAround(InterceptInfo interceptInfo){
+//        Logger.simple("Around========>" + JSON.toJSON(interceptInfo));
+        Object object = null;
+        try {
+            object = interceptInfo.process();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        return "-----////" + object;
     }
 }
