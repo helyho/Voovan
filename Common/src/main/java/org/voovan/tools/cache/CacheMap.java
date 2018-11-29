5466a1f0b93918213d024bcc6aa4c3fd09b6c6e1
helyho <helyho@gmail.com>
2018-11-30 04:03:34 +0800

    imp: 优化缓存接口 CacheMap / RedisMap / CachedHashMap

--- Common/src/main/java/org/voovan/tools/cache/CacheMap.java
+++ Common/src/main/java/org/voovan/tools/cache/CacheMap.java
@@ -1,6 +1,7 @@
 package org.voovan.tools.cache;
 
 import java.util.Map;
+import java.util.function.Function;
 
 /**
  * 缓存的基础类
@@ -17,4 +18,89 @@ public interface CacheMap<K, V> extends Map<K,V>{
     public V putIfAbsent(K key, V value, long expire);
 
     public void putAll(Map<? extends K, ? extends V> map, long expire);
+
+
+    /**
+     * 为 Key 获取 key 的超时时间
+     * @param key  key 名称
+     * @return 超时时间
+     */
+    public long expire(K key);
+
+    /**
+     * 更新某个对象的超时时间
+     *      可以为某个没有配置超时时间的键值对配置超时时间
+     * @param key 键
+     * @param expire 超时时间
+     */
+    public boolean expire(K key, long expire);
+
+
+    /**
+     * 获取对象
+     *      不会刷新对象的时间戳
+     * @param key 键
+     * @param appointedSupplier 指定的获取器
+     * @param createExpire 超时时间
+     * @param refresh 刷新超时时间
+     * @return 值
+     */
+    public V get(Object key, Function<K, V> appointedSupplier, Long createExpire, boolean refresh);
+
+
+    default V get(Object key, Function<K, V> appointedSupplier, Long expire) {
+        return get(key, appointedSupplier, expire, false);
+    }
+
+    /**
+     * 获取对象
+     *      不会刷新对象的时间戳
+     * @param key 键
+     * @param appointedSupplier 指定的获取器
+     * @return 值
+     */
+    default V get(Object key, Function<K, V> appointedSupplier){
+        return get(key, appointedSupplier, null, false);
+    }
+
+    /**
+     * 获取对象
+     *      不会刷新对象的时间戳
+     * @param key 键
+     * @return 值
+     */
+    default V get(Object key){
+        return get(key, null, null, false);
+    }
+
+    /**
+     * 获取并刷新对象
+     * @param key 键
+     * @param appointedSupplier 指定数据构造器
+     * @param expire 超时时间
+     * @return 值
+     */
+    default V getAndRefresh(Object key, Function<K, V> appointedSupplier, Long expire){
+        return get(key, appointedSupplier, expire, true);
+    }
+
+    /**
+     * 获取并刷新对象
+     * @param key 键
+     * @param appointedSupplier 指定数据构造器
+     * @return 值
+     */
+    default V getAndRefresh(Object key, Function<K, V> appointedSupplier){
+        return get(key, appointedSupplier, null, true);
+    }
+
+    /**
+     * 获取并刷新对象
+     * @param key 键
+     * @return 值
+     */
+    default V getAndRefresh(Object key){
+        return get(key, null, null, true);
+    }
 }
+
