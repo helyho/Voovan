e7eb056f4f2b68f09bd53984b1804c89bcd9b05d
helyho <helyho@gmail.com>
2018-11-29 22:25:51 +0800

    fix: TSQL 修复查询条件的参数字段中带有 limit 等关键字导致的问题

--- Database/src/test/java/org/voovan/test/tools/TSQLUnit.java
+++ Database/src/test/java/org/voovan/test/tools/TSQLUnit.java
@@ -11,13 +11,18 @@ package org.voovan.tools;
 public class TSQLUnit {
 
     public static void main(String[] args) {
-        String s = "select * from deposit_request where status != 2 AND `random_deposit_amountx` = ::5 \n" +
+        String s = "select * from deposit_request where status != 2 \n" +
+                "                            and onceDrawLimit = ::onceDrawLimit \n" +
+                "                            AND `random_deposit_amountx` = ::kkand \n" +
                 "							 and  mmmk between ::1 and ::4 \n" +
                 "							 and  mmm not in (::10, ::11, ::12, ::13)\n" +
                 "							 and  mmm in (::10, ::11, ::12, ::13)\n" +
                 "                            and  (client_account_name >= ::1 or client_card_number <= ::2) \n" +
                 "                            and  (client_account_name != ::3 or client_card_numbe1 > ::4) \n" +
                 "                            and  (post_script >= ::6 or deposit_amount = ::7 or random_deposit_amount = ::8)\n" +
+                "                            and  (select * from xx where kk=::6)\n" +
+                "                            and  exists (select * from xx where kk=::6)\n" +
+                "                            and  not exists (select * from xx where kk=::6)\n" +
                 "                            and state = 1";
 
 //		String s = "SELECT count(0) from 90_entrust where 1=1 and status in (0,1,2,3) and user_id = '7hJvVarCrMe' and original_market_id = '90' and type in (0,1) and xx is null";
@@ -27,6 +32,15 @@ public class TSQLUnit {
 //			e.printStackTrace();
 //		}
 
-        System.out.println(TSQL.removeEmptyCondiction(s, TObject.asMap("1", null, "4", null, "7", null, "10", null)));
+        System.out.println(TSQL.removeEmptyCondiction(s, TObject.asMap("1", null, "4", null, "7", null, "10", null, "11", null)));
+
+
+//        s = "select * from ec_menu em where 1=1 \n" +
+//                "                        and `menu_id`=::menuId \n" +
+//                "                        and `t1.menu_name` like CONCAT('%', CONCAT( ::menuName,'%')) \n" +
+//                "                        and em.state=1 and em.parent_menu_id=0 limit ::pageStart , ::pageSize";
+//
+//        System.out.println(s);
+//        System.out.println(TSQL.removeEmptyCondiction(s, TObject.asMap("menuId", "mid", /*"menuName", "mN",*/ "pageStart", 2, "pageSize", 100)));
     }
 }
