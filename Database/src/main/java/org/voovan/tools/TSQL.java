bb8af0371e9f4de3b748ddec95fb22dae5109c33
helyho <helyho@gmail.com>
2018-12-05 17:40:15 +0800

    imp: 优化 TSQL

--- Database/src/main/java/org/voovan/tools/TSQL.java
+++ Database/src/main/java/org/voovan/tools/TSQL.java
@@ -430,6 +430,9 @@ public class TSQL {
 				condictionArr[1] = condictionArr[1].trim();
 
 				if(condictionArr.length>1){
+					if(operatorChar.contains("in") && condictionArr[1].trim().startsWith("(") && condictionArr[1].trim().endsWith(")")){
+						condictionArr[1] = condictionArr[1].substring(1,condictionArr[1].length()-1);
+					}
 
 					if(condictionArr[0].startsWith("(") && TString.regexMatch(condictionArr[1], "\\(") > TString.regexMatch(condictionArr[1], "\\)")){
 						condictionArr[0] = TString.removePrefix(condictionArr[0]);
