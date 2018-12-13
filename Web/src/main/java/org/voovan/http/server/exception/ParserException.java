b09d4c76ad37f4ee53d3f8ebada827752c71cb8e
helyho <helyho@gmail.com>
2018-12-13 20:12:50 +0800

    imp: HttpParser 重写, 减少性能消耗

---
+++ Web/src/main/java/org/voovan/http/server/exception/ParserException.java
@@ -0,0 +1,17 @@
+package org.voovan.http.server.exception;
+
+import java.io.IOException;
+
+/**
+ * 类文字命名
+ *
+ * @author: helyho
+ * Voovan Framework.
+ * WebSite: https://github.com/helyho/Voovan
+ * Licence: Apache v2 License
+ */
+public class ParserException extends IOException {
+    public ParserException(String msg){
+        super(msg);
+    }
+}
