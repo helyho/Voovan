68c69d5f5d079bd03a3e1c14edd2de1cfed89452
helyho <helyho@gmail.com>
2018-12-19 20:33:12 +0800

    imp: NioSelector 优化

--- Network/src/main/java/org/voovan/network/nio/NioSelector.java
+++ Network/src/main/java/org/voovan/network/nio/NioSelector.java
@@ -5,6 +5,7 @@ import org.voovan.network.*;
 import org.voovan.tools.ByteBufferChannel;
 import org.voovan.tools.TByteBuffer;
 import org.voovan.tools.log.Logger;
+import org.voovan.tools.threadpool.ThreadPool;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -13,6 +14,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * 事件监听器
@@ -24,7 +26,6 @@ import java.util.concurrent.ConcurrentLinkedDeque;
  * Licence: Apache v2 License
  */
 public class NioSelector {
-
 	public static ConcurrentLinkedDeque<NioSelector> SELECTORS = new ConcurrentLinkedDeque<NioSelector>();
 	static {
 		Global.getThreadPool().execute(()->{
@@ -44,7 +45,6 @@ public class NioSelector {
 	private ByteBufferChannel sessionByteBufferChannel;
 	private ByteBufferChannel tmpByteBufferChannel;
 	private ByteBuffer readTempBuffer;
-	private int waitCount = 0;
 
 	private NioSession session;
 
@@ -89,10 +89,9 @@ public class NioSelector {
 			if (socketContext != null && socketContext.isConnected()) {
 				if (selector.select(1) > 0) {
 					Set<SelectionKey> selectionKeys = selector.selectedKeys();
-					Iterator<SelectionKey> selectionKeyIterator = selectionKeys
-							.iterator();
+					Iterator<SelectionKey> selectionKeyIterator = selectionKeys.iterator();
+
 					while (selectionKeyIterator.hasNext()) {
-						waitCount = 0;
 						SelectionKey selectionKey = selectionKeyIterator.next();
 						if (selectionKey.isValid()) {
 							// 获取 socket 通道
@@ -100,74 +99,83 @@ public class NioSelector {
 							if (socketChannel.isOpen() && selectionKey.isValid()) {
 								// 事件分发,包含时间 onRead onAccept
 
-								switch (selectionKey.readyOps()) {
-									// Server接受连接
-									case SelectionKey.OP_ACCEPT: {
-										NioServerSocket serverSocket = (NioServerSocket)socketContext;
-										NioSocket socket = new NioSocket(serverSocket,socketChannel);
-										EventTrigger.fireAccept(socket.getSession());
-										break;
-									}
-									// 有数据读取
-									case SelectionKey.OP_READ: {
-										int readSize = socketChannel.read(readTempBuffer);
-
-										//判断连接是否关闭
-										if(MessageLoader.isStreamEnd(readTempBuffer, readSize) && session.isConnected()){
-
-											session.getMessageLoader().setStopType(MessageLoader.StopType.STREAM_END);
-											session.close();
-											break;
-										}else if(readSize>0){
-											readTempBuffer.flip();
-
-											tmpByteBufferChannel.clear();
-
-											//接收SSL数据, SSL握手完成后解包
-											if(session.getSSLParser()!=null && SSLParser.isHandShakeDone(session)){
-												//一次接受并完成 SSL 解码后, 常常有剩余无法解码数据, 所以用 netByteBufferChannel 这个通道进行保存
-												netByteBufferChannel.writeEnd(readTempBuffer);
-												session.getSSLParser().unWarpByteBufferChannel(session, netByteBufferChannel, tmpByteBufferChannel);
+								Global.getThreadPool().execute(()->{
+									try {
+										switch (selectionKey.readyOps()) {
+											// Server接受连接
+											case SelectionKey.OP_ACCEPT: {
+												NioServerSocket serverSocket = (NioServerSocket) socketContext;
+												NioSocket socket = new NioSocket(serverSocket, socketChannel);
+												EventTrigger.fireAccept(socket.getSession());
+												break;
 											}
+											// 有数据读取
+											case SelectionKey.OP_READ: {
+												int readSize = socketChannel.read(readTempBuffer);
 
-											//如果在没有 SSL 支持 和 握手没有完成的情况下,直接写入
-											if(session.getSSLParser()==null || !SSLParser.isHandShakeDone(session)){
-												tmpByteBufferChannel.writeEnd(readTempBuffer);
+												//判断连接是否关闭
+												if (MessageLoader.isStreamEnd(readTempBuffer, readSize) && session.isConnected()) {
+
+													session.getMessageLoader().setStopType(MessageLoader.StopType.STREAM_END);
+													session.close();
+													break;
+												} else if (readSize > 0) {
+													readTempBuffer.flip();
+
+													tmpByteBufferChannel.clear();
+
+													//接收SSL数据, SSL握手完成后解包
+													if (session.getSSLParser() != null && SSLParser.isHandShakeDone(session)) {
+														//一次接受并完成 SSL 解码后, 常常有剩余无法解码数据, 所以用 netByteBufferChannel 这个通道进行保存
+														netByteBufferChannel.writeEnd(readTempBuffer);
+														session.getSSLParser().unWarpByteBufferChannel(session, netByteBufferChannel, tmpByteBufferChannel);
+													}
+
+													//如果在没有 SSL 支持 和 握手没有完成的情况下,直接写入
+													if (session.getSSLParser() == null || !SSLParser.isHandShakeDone(session)) {
+														tmpByteBufferChannel.writeEnd(readTempBuffer);
+													}
+
+													//检查心跳
+													if (SSLParser.isHandShakeDone(session)) {
+														HeartBeat.interceptHeartBeat(session, tmpByteBufferChannel);
+													}
+
+													if (tmpByteBufferChannel.size() > 0) {
+														sessionByteBufferChannel.writeEnd(tmpByteBufferChannel.getByteBuffer());
+														tmpByteBufferChannel.compact();
+
+														// 触发 onReceive 事件
+														EventTrigger.fireReceiveThread(session);
+													}
+												}
+
+												// 接收完成后重置buffer对象
+												readTempBuffer.clear();
+
+												break;
 											}
-
-											//检查心跳
-											if(SSLParser.isHandShakeDone(session)) {
-												HeartBeat.interceptHeartBeat(session, tmpByteBufferChannel);
-											}
-
-											if(tmpByteBufferChannel.size() > 0) {
-												sessionByteBufferChannel.writeEnd(tmpByteBufferChannel.getByteBuffer());
-												tmpByteBufferChannel.compact();
-
-												// 触发 onReceive 事件
-												EventTrigger.fireReceiveThread(session);
+											default: {
+												Logger.fremawork("Nothing to do ,SelectionKey is:"
+														+ selectionKey.readyOps());
 											}
 										}
-
-										// 接收完成后重置buffer对象
-										readTempBuffer.clear();
-
-										break;
+										selectionKeyIterator.remove();
+									} catch (Exception e) {
+										if(e instanceof IOException){
+											session.close();
+										}
+										//兼容 windows 的 "java.io.IOException: 指定的网络名不再可用" 错误
+										else if(e.getStackTrace()[0].getClassName().contains("sun.nio.ch")){
+											return;
+										} else if(e instanceof Exception){
+											//触发 onException 事件
+											EventTrigger.fireExceptionThread(session, e);
+										}
 									}
-									default: {
-										Logger.fremawork("Nothing to do ,SelectionKey is:"
-												+ selectionKey.readyOps());
-									}
-								}
-								selectionKeyIterator.remove();
+								});
 							}
 						}
-					}
-				} else {
-					//ReadTimeout 控制
-					waitCount++;
-					if(socketContext instanceof NioSocket && waitCount >= socketContext.getReadTimeout()) {
-						session.close();
 					}
 				}
 			}
---
+++ Network/src/test/java/org/voovan/test/network/nio/NioServerSocketBenchTest.java
@@ -0,0 +1,16 @@
+package org.voovan.test.network.nio;
+
+import org.voovan.network.aio.AioServerSocket;
+import org.voovan.network.nio.NioServerSocket;
+import org.voovan.test.network.ServerBenchHandlerTest;
+
+import java.io.IOException;
+
+public class NioServerSocketBenchTest {
+
+	public static void main(String[] args) throws IOException {
+		NioServerSocket serverSocket = new NioServerSocket("127.0.0.1",28080,30*1000);
+		serverSocket.handler(new ServerBenchHandlerTest());
+		serverSocket.start();
+	}
+}
