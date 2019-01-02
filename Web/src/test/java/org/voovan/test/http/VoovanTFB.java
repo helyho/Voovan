5207ef6b6c297721da477b4a15d32daa2b3a7a56
helyho <helyho@gmail.com>
2019-01-02 17:20:57 +0800

    add: 新增 TFB 测试用例

---
+++ Web/src/test/java/org/voovan/test/http/VoovanTFB.java
@@ -0,0 +1,31 @@
+package org.voovan.test.http;
+
+import org.voovan.http.server.HttpRequest;
+import org.voovan.http.server.HttpResponse;
+import org.voovan.http.server.HttpRouter;
+import org.voovan.http.server.WebServer;
+import org.voovan.http.server.context.WebContext;
+import org.voovan.http.server.context.WebServerConfig;
+import org.voovan.tools.log.Logger;
+
+
+public class VoovanTFB {
+
+	public static void main(String[] args) {
+		WebServerConfig webServerConfig = WebContext.getWebServerConfig();
+		webServerConfig.setGzip(false);
+		webServerConfig.setAccessLog(false);
+		WebServer webServer = WebServer.newInstance(webServerConfig);
+		Logger.setState(false);
+
+		//性能测试请求
+		webServer.get("/plaintext", new HttpRouter() {
+			public void process(HttpRequest req, HttpResponse resp) throws Exception {
+				resp.header().put("Content-Type", "text/plain");
+				resp.write("Hello, World!");
+			}
+		});
+
+		webServer.serve();
+	}
+}
