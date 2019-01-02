d80ab2a58db6d4e32dffc18f0fadba5c6c907b99
helyho <helyho@gmail.com>
2019-01-02 16:48:18 +0800

    add: 新增 Undertow 的测试用例

--- Web/src/test/java/org/voovan/test/http/UnderTowBench.java
+++ Web/src/test/java/org/voovan/test/http/UnderTowBench.java
@@ -4,6 +4,9 @@ import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
+import org.voovan.tools.ByteBufferChannel;
+
+import java.nio.ByteBuffer;
 
 /**
  * 类文字命名
@@ -15,13 +18,14 @@ import io.undertow.util.Headers;
  */
 public class UnderTowBench {
     public static void main(final String[] args) {
+        String tmp="OK";
         Undertow server = Undertow.builder()
                 .addHttpListener(8080, "localhost")
                 .setHandler(new HttpHandler() {
                     @Override
                     public void handleRequest(final HttpServerExchange exchange) throws Exception {
                         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                        exchange.getResponseSender().send("Hello World");
+                        exchange.getResponseSender().send(tmp);
                     }
                 }).build();
         server.start();
