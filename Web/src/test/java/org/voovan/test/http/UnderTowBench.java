04e1ba7759d1be2c6d651479ed32b5b56e43c99b
helyho <helyho@gmail.com>
2019-01-02 16:45:28 +0800

    add: 新增 Undertow 的测试用例

---
+++ Web/src/test/java/org/voovan/test/http/UnderTowBench.java
@@ -0,0 +1,29 @@
+package org.voovan.test.http;
+
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+/**
+ * 类文字命名
+ *
+ * @author: helyho
+ * Voovan Framework.
+ * WebSite: https://github.com/helyho/Voovan
+ * Licence: Apache v2 License
+ */
+public class UnderTowBench {
+    public static void main(final String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+                    @Override
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello World");
+                    }
+                }).build();
+        server.start();
+    }
+}
