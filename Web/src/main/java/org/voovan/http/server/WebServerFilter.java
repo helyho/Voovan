b24cff3508fd16b6948d17e1cd5877504411ed59
helyho <helyho@gmail.com>
2019-01-28 22:23:27 +0800

    imp: HttpParser.parserProtocol 优化

--- Web/src/main/java/org/voovan/http/server/WebServerFilter.java
+++ Web/src/main/java/org/voovan/http/server/WebServerFilter.java
@@ -74,7 +74,7 @@ public class WebServerFilter implements IoFilter {
 							RESPONSE_CACHE.put(cacheMark, cacheBytes);
 						}
 					} else {
-						if(session.getReadByteBufferChannel().size() > 0 && RESPONSE_CACHE.size() == 1){
+						if(session.getReadByteBufferChannel().size() > 0){
 							session.sendByBuffer(ByteBuffer.wrap(cacheBytes));
 						}
 						session.sendByBuffer(ByteBuffer.wrap(cacheBytes));
