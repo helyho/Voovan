4b3c7f5e129465aa6524479e9e596c9df7eb6ea4
helyho <helyho@gmail.com>
2019-03-12 15:09:05 +0800

    imp: 优化 RingBuffer 并行支持

--- Common/src/test/java/org/voovan/test/tools/RingBufferUnit.java
+++ Common/src/test/java/org/voovan/test/tools/RingBufferUnit.java
@@ -1,12 +1,17 @@
 package org.voovan.test.tools;
 
+import com.sun.org.apache.bcel.internal.generic.ARRAYLENGTH;
 import junit.framework.TestCase;
+import org.voovan.Global;
 import org.voovan.tools.DirectRingBuffer;
 import org.voovan.tools.RingBuffer;
 import org.voovan.tools.TEnv;
+import sun.jvm.hotspot.memory.TenuredGeneration;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 类文字命名
@@ -42,4 +47,83 @@ public class RingBufferUnit extends TestCase {
 		ringBuffer.pop(a, 0, a.length);
 		TEnv.sleep(100000);
 	}
+
+	public static AtomicInteger m1 = new AtomicInteger(0);
+	public static AtomicInteger m2 = new AtomicInteger(0);
+	public static AtomicInteger md = new AtomicInteger(0);
+
+	public void testConcurrent(){
+		RingBuffer m = new RingBuffer(1024*1024);
+		System.out.println(TEnv.measureTime(new Runnable() {
+			@Override
+			public void run() {
+
+				ArrayList<Thread> mmm = new ArrayList<Thread>();
+
+				String ms ="";
+
+				Thread mt1 = new Thread(()->{
+					while(md.get()<=12000000) {
+						int data = md.incrementAndGet();
+						m.push(data);
+						m2.getAndIncrement();
+					}
+				});
+
+				Thread mt2 = new Thread(()->{
+					while(m1.get()<12000000) {
+						Object o = m.pop();
+						if (o != null) {
+							m1.getAndIncrement();
+						}
+					}
+				});
+
+				mmm.add(mt1);
+				mmm.add(mt2);
+
+				mt1.start();
+				mt2.start();
+
+		//				for(int i=0;i<150;i++){
+//					Thread mt = new Thread(()->{
+//						while(true) {
+//							if (Math.random() > 0.5 && m.remaining() > 0) {
+//								Object o = m.pop();
+//								if(o !=null) {
+////							System.out.println("pop: " + o);
+//									m1.getAndIncrement();
+//								}
+//							} else {
+//								if(md.get()<10000000) {
+//									int data = md.incrementAndGet();
+//									m.push(data);
+//									m2.getAndIncrement();
+////							System.out.println("push: " + data);
+//								} else {
+//									break;
+//								}
+//							}
+//						}
+//					});
+//					mmm.add(mt);
+//					mt.start();
+//				}
+
+				for(Thread thread : mmm){
+					try {
+						thread.join();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+
+			}
+		}));
+
+		System.out.println(m1.get() + " " + m2.get() + " " + m.remaining());
+
+	}
 }
+
+
