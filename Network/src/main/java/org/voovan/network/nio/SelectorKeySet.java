a5bf347e17685bbf6375931203e01d59b0026642
helyho <helyho@gmail.com>
2019-03-19 14:54:47 +0800

    imp: 优化 Selector

---
+++ Network/src/main/java/org/voovan/network/nio/SelectorKeySet.java
@@ -0,0 +1,70 @@
+package org.voovan.network.nio;
+
+import java.nio.channels.SelectionKey;
+import java.util.AbstractSet;
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * 类文字命名
+ *
+ * @author: helyho
+ * Voovan Framework.
+ * WebSite: https://github.com/helyho/Voovan
+ * Licence: Apache v2 License
+ */
+public class SelectorKeySet extends AbstractSet<SelectionKey> {
+
+	SelectionKey[] selectionKeys;
+	volatile int size;
+
+	public SelectorKeySet(int cap) {
+		selectionKeys = new SelectionKey[cap];
+	}
+
+	public SelectionKey[] getSelectionKeys() {
+		return selectionKeys;
+	}
+
+	@Override
+	public boolean add(SelectionKey selectionKey) {
+		selectionKeys[size++] = selectionKey;
+		if (selectionKeys.length == size) {
+			reallocate();
+		}
+		return true;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		return false;
+	}
+
+	private void reallocate() {
+		selectionKeys = Arrays.copyOf(selectionKeys, selectionKeys.length << 1);
+	}
+
+	@Override
+	public Iterator<SelectionKey> iterator() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		return false;
+	}
+
+	void reset() {
+		size = 0;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public String toString() {
+		return "SelectionKeySet.size: " + size();
+	}
+}
