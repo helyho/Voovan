a49dfaa795741b212c7789e383743a1d12dcf0ba
helyho <helyho@gmail.com>
2019-03-29 15:11:07 +0800

    imp: VoovanTFB 测试用例优化

--- Web/src/test/java/org/voovan/test/http/VoovanTFB.java
+++ Web/src/test/java/org/voovan/test/http/VoovanTFB.java
@@ -1,5 +1,9 @@
 package org.voovan.test.http;
 
+import com.jsoniter.output.JsonStream;
+import com.jsoniter.output.JsonStreamPool;
+import com.jsoniter.spi.JsonException;
+import org.voovan.http.message.HttpStatic;
 import org.voovan.http.server.HttpRequest;
 import org.voovan.http.server.HttpResponse;
 import org.voovan.http.server.HttpRouter;
@@ -10,13 +14,27 @@ import org.voovan.tools.TObject;
 import org.voovan.tools.json.JSON;
 import org.voovan.tools.log.Logger;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
 
 
 public class VoovanTFB {
-	public static final byte[] HELLO_WORLD = "Hello, World!".getBytes();
-	public static final Map MAP = TObject.asMap("message", "Hello, World!");
+	private static final byte[] HELLO_WORLD = "Hello, World!".getBytes();
 
+	static class Message {
+
+		private final String message;
+
+		public Message(String message) {
+			this.message = message;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+	}
 
 	public static void main(String[] args) {
 
@@ -25,7 +43,7 @@ public class VoovanTFB {
 		webServerConfig.setAccessLog(false);
 		webServerConfig.setKeepAliveTimeout(1000);
 		webServerConfig.setHost("0.0.0.0");
-		webServerConfig.setPort(28080);
+		webServerConfig.setPort(8080);
 		webServerConfig.setHotSwapInterval(0);
 		webServerConfig.setCache(true);
 		webServerConfig.getModuleonfigs().clear();
@@ -36,19 +54,38 @@ public class VoovanTFB {
 		//性能测试请求;
 		webServer.get("/plaintext", new HttpRouter() {
 			public void process(HttpRequest req, HttpResponse resp) throws Exception {
-				resp.header().put("Content-Type", "text/plain");
+				resp.header().put(HttpStatic.CONTENT_TYPE_STRING, "text/plain");
 				resp.write(HELLO_WORLD);
+			}
+		});
+		//性能测试请求
+		webServer.get("/vjson", new HttpRouter() {
+			public void process(HttpRequest req, HttpResponse resp) throws Exception {
+				resp.header().put("Content-Type", "application/json");
+				resp.write(JSON.toJSON(TObject.asMap("message", "Hello, World!"), false, false));
 			}
 		});
 		//性能测试请求
 		webServer.get("/json", new HttpRouter() {
 			public void process(HttpRequest req, HttpResponse resp) throws Exception {
 				resp.header().put("Content-Type", "application/json");
-				resp.write(JSON.toJSON(MAP, false, false));
+				resp.write(serializeMsg(new Message("Hello, World!")));
 			}
 		});
 
-
 		webServer.syncServe();
+	}
+
+	private static byte[] serializeMsg(Message obj) {
+		JsonStream stream = JsonStreamPool.borrowJsonStream();
+		try {
+			stream.reset(null);
+			stream.writeVal(Message.class, obj);
+			return Arrays.copyOfRange(stream.buffer().data(), 0, stream.buffer().tail());
+		} catch (IOException e) {
+			throw new JsonException(e);
+		} finally {
+			JsonStreamPool.returnJsonStream(stream);
+		}
 	}
 }
