1cc9bccc2c5d83cfc32025a47016da6954e29998
helyho <helyho@gmail.com>
2019-06-14 18:31:09 +0800

    imp: ParserException 重命名为SocketIOParserException

--- Web/src/main/java/org/voovan/http/extend/SocketIOParserException.java
+++ Web/src/main/java/org/voovan/http/extend/SocketIOParserException.java
@@ -8,8 +8,8 @@ package org.voovan.http.extend;
  * WebSite: https://github.com/helyho/Voovan
  * Licence: Apache v2 License
  */
-public class ParserException extends Exception{
-    public ParserException(String msg){
+public class SocketIOParserException extends Exception{
+    public SocketIOParserException(String msg){
         super(msg);
     }
 }
