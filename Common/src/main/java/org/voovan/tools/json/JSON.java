0a10692d77bd26dbd4ff57ca0952dece6e0aa85b
helyho <helyho@gmail.com>
2019-07-23 15:14:21 +0800

    fix: 修复配置文件不存在时的异常

--- Common/src/main/java/org/voovan/tools/json/JSON.java
+++ Common/src/main/java/org/voovan/tools/json/JSON.java
@@ -20,7 +20,7 @@ import java.util.function.Supplier;
  * Licence: Apache v2 License
  */
 public class JSON {
-	public final static String JSON_CONVERT_ESCAPE_CHAR = TProperties.getString("framework", "JSONConvertEscapeChar");
+	public final static String JSON_CONVERT_ESCAPE_CHAR = TProperties.getString("framework", "JSONConvertEscapeChar", "true");
 
 	/**
 	 * 是否进行 EscapeChar 的转换, 默认 true, 当你确定你不存在字符串多行换行的时候可设置为 false
