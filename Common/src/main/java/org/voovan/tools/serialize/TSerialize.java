2f14da57a9df0e8e24f2877b19c2a9a7dc2db17f
helyho <helyho@gmail.com>
2019-07-30 20:20:07 +0800

    imp: TSerialize 小优化

--- Common/src/main/java/org/voovan/tools/serialize/TSerialize.java
+++ Common/src/main/java/org/voovan/tools/serialize/TSerialize.java
@@ -109,7 +109,6 @@ public class TSerialize {
         CLASS_AND_HASH.put(boolean.class,   THash.HashFNV1(Boolean.class.getName()));
         CLASS_AND_HASH.put(Boolean.class,   THash.HashFNV1(Boolean.class.getName()));
         CLASS_AND_HASH.put(String.class,    THash.HashFNV1(String.class.getName()));
-        CLASS_AND_HASH.put(byte[].class,    THash.HashFNV1(byte[].class.getName()));
 
         for(Map.Entry<Class, Integer> entry : CLASS_AND_HASH.entrySet()) {
 
@@ -117,7 +116,6 @@ public class TSerialize {
                 HASH_AND_CLASS.put(entry.getValue(), entry.getKey());
             }
         }
-
     }
 
     protected static synchronized Integer getHashByClass(Class clazz){
