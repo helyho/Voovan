2c439636bb14d6088ebe8775ae8b8ab36235d65b
helyho <helyho@gmail.com>
2019-08-08 12:41:16 +0800

    fix: 修复 ObjectPool.restitution 中 validator 为空时的异常

--- Common/src/test/java/org/voovan/test/Other.java
+++ Common/src/test/java/org/voovan/test/Other.java
@@ -1,6 +1,8 @@
 package org.voovan.test;
 
 import org.voovan.tools.TByte;
+import org.voovan.tools.TDateTime;
+import org.voovan.tools.UniqueId;
 import org.voovan.tools.reflect.TReflect;
 import org.voovan.tools.serialize.TSerialize;
 
@@ -17,6 +19,16 @@ public class Other {
     private int orange = 10;
 
     public static void main(String[] args) throws Exception {
-        System.out.println(new String(TByte.getBytes(12312312)));
+        final UniqueId uniqueId = new UniqueId(200);
+        System.out.println("1970 " + uniqueId.getNumber(TDateTime.parse("1970-1-1 00:00:00").getTime()));
+        System.out.println("1980 " + uniqueId.getNumber(TDateTime.parse("1980-1-1 00:00:00").getTime()));
+        System.out.println("1990 " + uniqueId.getNumber(TDateTime.parse("1990-1-1 00:00:00").getTime()));
+        System.out.println("2000 " + uniqueId.getNumber(TDateTime.parse("2000-1-1 00:00:00").getTime()));
+        System.out.println("2010 " + uniqueId.getNumber(TDateTime.parse("2010-1-1 00:00:00").getTime()));
+        System.out.println("2020 " + uniqueId.getNumber(TDateTime.parse("2020-1-1 00:00:00").getTime()));
+        System.out.println("2030 " + uniqueId.getNumber(TDateTime.parse("2030-1-1 00:00:00").getTime()));
+        System.out.println("2040 " + uniqueId.getNumber(TDateTime.parse("2040-1-1 00:00:00").getTime()));
+        System.out.println(6617806405632409601L >> 22);
+
     }
 }
