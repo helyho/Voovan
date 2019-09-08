c9500bbac88592aaeb0c653029400b95c037133d
helyho <helyho@gmail.com>
2019-09-08 16:42:42 +0800

    add: 新增限流队列

---
+++ Common/src/main/java/org/voovan/tools/collection/FixedQueue.java
@@ -0,0 +1,114 @@
+package org.voovan.tools.collection;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+/**
+ * 定长队列
+ *  当队列满时,会自动清除头部元素
+ *
+ * @author: zhengtao
+ * voovan Framework.
+ * WebSite: https://github.com/helyho/voovan
+ * Licence: Apache v2 License
+ */
+public class FixedQueue<E> {
+
+    private static final long serialVersionUID = -6271813154993569614L;
+    /**
+     * 队列长度
+     */
+    private int limit;
+
+    private LinkedList<E> queue = new LinkedList<E>();
+
+    public FixedQueue(int limit) {
+        this.limit = limit;
+    }
+
+    /**
+     * 入列：当队列大小已满时，把队头的元素poll掉，将数据插入队尾
+     *
+     * @param e 入队的元素
+     */
+    public void offer(E e) {
+        int i = 0;
+        while (queue.size() >= limit) {
+            queue.poll();
+            i++;
+        }
+        queue.offer(e);
+    }
+
+    public void set(E e, int position) {
+        queue.set(position, e);
+    }
+
+    public E get(int position) {
+        return queue.get(position);
+    }
+
+    /**
+     * 清空队列
+     */
+    public void clearAll() {
+        this.queue.clear();
+    }
+
+    public E getLast() {
+        return queue.peekLast();
+    }
+
+    public E getFirst() {
+        return queue.peekFirst();
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    public boolean isEmpty(){
+        return queue.isEmpty();
+    }
+
+    public E removeLast() {
+        return queue.removeLast();
+    }
+
+    public E removeFirst() {
+        return queue.removeFirst();
+    }
+
+    public boolean removeFirst(E e) {
+        return queue.remove(e);
+    }
+
+    public E removeIndex(int index) {
+        return queue.remove(index);
+    }
+
+    public void addIndex(int index, E object) {
+        queue.add(index, object);
+        int i = 0;
+        while (queue.size() > limit) {
+            queue.poll();
+            i++;
+        }
+    }
+
+    public FixedQueue<E> clone() {
+        FixedQueue<E> newOne = new FixedQueue<E>(this.limit);
+        newOne.addAll(queue);
+        return newOne;
+    }
+
+    public void addAll(Collection<E> datas) {
+        for(E e : datas) {
+            offer(e);
+        }
+    }
+}
