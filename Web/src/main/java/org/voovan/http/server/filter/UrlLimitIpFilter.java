b60d8231e8fe8a9b377b7ba84c93bca87ce6d2b4
helyho <helyho@gmail.com>
2019-10-28 19:44:50 +0800

    add: 新增 UrlLimitIpFilter 根据Url限定ip访问的过滤器

---
+++ Web/src/main/java/org/voovan/http/server/filter/UrlLimitIpFilter.java
@@ -0,0 +1,70 @@
+package org.voovan.http.server.filter;
+
+import org.voovan.http.server.HttpFilter;
+import org.voovan.http.server.HttpRequest;
+import org.voovan.http.server.HttpResponse;
+import org.voovan.http.server.context.HttpFilterConfig;
+import org.voovan.tools.TObject;
+import org.voovan.tools.log.Logger;
+
+import java.util.List;
+
+/**
+ * 根据 Url 限定 ip 访问的过滤器
+ *
+ * @author: helyho
+ * voovan Framework.
+ * WebSite: https://github.com/helyho/voovan
+ * Licence: Apache v2 License
+ */
+public class UrlLimitIpFilter implements HttpFilter {
+    @Override
+    public Object onRequest(HttpFilterConfig filterConfig, HttpRequest request, HttpResponse response, Object prevFilterResult) {
+        List<String> ipList = (List<String>) filterConfig.getParameter("IpList");
+        List<String> urlList= (List<String>) filterConfig.getParameter("UrlList");
+        Boolean invalidClose = (Boolean) TObject.nullDefault(filterConfig.getParameter("invalidClose"), true);
+
+
+        String remoteAddress = request.getRemoteAddres();
+        String path = request.protocol().getPath();
+
+        //检查地址
+        boolean matchUrl = false;
+        for (String url : urlList) {
+            if (path.toLowerCase().startsWith(url.toLowerCase() )) {
+                matchUrl = true;
+                break;
+            }
+        }
+
+        //检查 ip
+        boolean matchIp = false;
+        if(matchUrl) {
+            for (String address : ipList) {
+                if (remoteAddress.startsWith(address)) {
+                    matchIp = true;
+                }
+            }
+        }
+
+
+        if(matchUrl) {
+            if(matchIp) {
+                return true;
+            } else {
+                if (invalidClose) {
+                    Logger.warn("Deny access: [url=" + path+ ", ip="+remoteAddress+"]");
+                    request.getSession().close();
+                }
+                return null;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public Object onResponse(HttpFilterConfig filterConfig, HttpRequest request, HttpResponse response, Object prevFilterResult) {
+        return null;
+    }
+}
