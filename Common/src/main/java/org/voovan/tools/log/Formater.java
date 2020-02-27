ad4956758999e8d1d30a43725e2268415c642098
helyho <helyho@gmail.com>
2020-02-27 14:07:34 +0800

    fix: 修复日志问题

--- Common/src/main/java/org/voovan/tools/log/Formater.java
+++ Common/src/main/java/org/voovan/tools/log/Formater.java
@@ -107,7 +107,7 @@ public class Formater {
      */
     public static StackTraceElement currentStackLine() {
         StackTraceElement[] stackTraceElements = TEnv.getStackElements();
-        return stackTraceElements[6];
+        return stackTraceElements[8];
     }
 
     /**
