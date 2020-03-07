3b2ef5e0d388d369d3f01e6529a94053196317df
helyho <helyho@gmail.com>
2020-03-07 19:09:40 +0800

    add: TByte 改成小端模式, 方便 byte[] 进行比较

--- Common/src/main/java/org/voovan/tools/TByte.java
+++ Common/src/main/java/org/voovan/tools/TByte.java
@@ -14,24 +14,24 @@ public class TByte {
 
     public static byte[] getBytes(short data) {
         byte[] bytes = new byte[2];
-        bytes[0] = (byte) (data & 0xff);
-        bytes[1] = (byte) ((data & 0xff00) >> 8);
+        bytes[1] = (byte) (data & 0xff);
+        bytes[0] = (byte) ((data & 0xff00) >> 8);
         return bytes;
     }
 
     public static byte[] getBytes(char data) {
         byte[] bytes = new byte[2];
-        bytes[0] = (byte) (data);
-        bytes[1] = (byte) (data >> 8);
+        bytes[1] = (byte) (data);
+        bytes[0] = (byte) (data >> 8);
         return bytes;
     }
 
     public static byte[] getBytes(int data) {
         byte[] bytes = new byte[4];
-        bytes[0] = (byte) (data & 0xff);
-        bytes[1] = (byte) ((data & 0xff00) >> 8);
-        bytes[2] = (byte) ((data & 0xff0000) >> 16);
-        bytes[3] = (byte) ((data & 0xff000000) >> 24);
+        bytes[3] = (byte) (data & 0xff);
+        bytes[2] = (byte) ((data & 0xff00) >> 8);
+        bytes[1] = (byte) ((data & 0xff0000) >> 16);
+        bytes[0] = (byte) ((data & 0xff000000) >> 24);
         return bytes;
     }
 
@@ -42,14 +42,14 @@ public class TByte {
 
     public static byte[] getBytes(long data) {
         byte[] bytes = new byte[8];
-        bytes[0] = (byte) (data & 0xff);
-        bytes[1] = (byte) ((data >> 8) & 0xff);
-        bytes[2] = (byte) ((data >> 16) & 0xff);
-        bytes[3] = (byte) ((data >> 24) & 0xff);
-        bytes[4] = (byte) ((data >> 32) & 0xff);
-        bytes[5] = (byte) ((data >> 40) & 0xff);
-        bytes[6] = (byte) ((data >> 48) & 0xff);
-        bytes[7] = (byte) ((data >> 56) & 0xff);
+        bytes[7] = (byte) (data & 0xff);
+        bytes[6] = (byte) ((data >> 8) & 0xff);
+        bytes[5] = (byte) ((data >> 16) & 0xff);
+        bytes[4] = (byte) ((data >> 24) & 0xff);
+        bytes[3] = (byte) ((data >> 32) & 0xff);
+        bytes[2] = (byte) ((data >> 40) & 0xff);
+        bytes[1] = (byte) ((data >> 48) & 0xff);
+        bytes[0] = (byte) ((data >> 56) & 0xff);
         return bytes;
     }
 
@@ -73,15 +73,15 @@ public class TByte {
     }
 
     public static short getShort(byte[] bytes) {
-        return (short) ((0xff & bytes[0]) | (0xff00 & (bytes[1] << 8)));
+        return (short) ((0xff & bytes[1]) | (0xff00 & (bytes[0] << 8)));
     }
 
     public static char getChar(byte[] bytes) {
-        return (char) ((0xff & bytes[0]) | (0xff00 & (bytes[1] << 8)));
+        return (char) ((0xff & bytes[1]) | (0xff00 & (bytes[0] << 8)));
     }
 
     public static int getInt(byte[] bytes) {
-        return (0xff & bytes[0]) | (0xff00 & (bytes[1] << 8)) | (0xff0000 & (bytes[2] << 16)) | (0xff000000 & (bytes[3] << 24));
+        return (0xff & bytes[3]) | (0xff00 & (bytes[2] << 8)) | (0xff0000 & (bytes[1] << 16)) | (0xff000000 & (bytes[0] << 24));
     }
 
     public static boolean getBoolean(byte[] bytes){
@@ -89,8 +89,10 @@ public class TByte {
     }
 
     public static long getLong(byte[] bytes) {
-        return (0xffL & (long) bytes[0]) | (0xff00L & ((long) bytes[1] << 8)) | (0xff0000L & ((long) bytes[2] << 16)) | (0xff000000L & ((long) bytes[3] << 24))
-                | (0xff00000000L & ((long) bytes[4] << 32)) | (0xff0000000000L & ((long) bytes[5] << 40)) | (0xff000000000000L & ((long) bytes[6] << 48)) | (0xff00000000000000L & ((long) bytes[7] << 56));
+        return (0xffL & (long) bytes[7]) | (0xff00L & ((long) bytes[6] << 8)) | (0xff0000L & ((long) bytes[5] << 16)) |
+                (0xff000000L & ((long) bytes[4] << 24)) | (0xff00000000L & ((long) bytes[3] << 32)) |
+                (0xff0000000000L & ((long) bytes[2] << 40)) | (0xff000000000000L & ((long) bytes[1] << 48)) |
+                (0xff00000000000000L & ((long) bytes[0] << 56));
     }
 
     public static float getFloat(byte[] bytes) {
