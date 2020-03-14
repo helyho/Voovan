78d6d3053b3a97dcadf938928acc7b22fc29ad91
helyho <helyho@gmail.com>
2020-03-15 02:51:47 +0800

    add: 新增 RocksMap.getProperty 方法 add: 新增 THash.murmurHash??? 算法

---
+++ Common/src/test/java/org/voovan/test/tools/security/THashUnit.java
@@ -0,0 +1,50 @@
+package org.voovan.test.tools.security;
+
+import junit.framework.TestCase;
+import org.voovan.tools.TByte;
+import org.voovan.tools.TEnv;
+import org.voovan.tools.UniqueId;
+import org.voovan.tools.security.THash;
+
+/**
+ * 类文字命名
+ *
+ * @author: helyho
+ * Voovan Framework.
+ * WebSite: https://github.com/helyho/Voovan
+ * Licence: Apache v2 License
+ */
+public class THashUnit extends TestCase {
+
+    public void testBench() {
+        UniqueId uniqueId = new UniqueId();
+        for(int x=0;x<3;x++) {
+            TEnv.measure("test-fnv="+x, () -> {
+                for (int i = 0; i < 500000; i++) {
+                    THash.HashFNV1(TByte.getBytes(uniqueId.nextNumber()), 0, 8);
+                }
+            });
+
+            TEnv.sleep(100);
+
+            TEnv.measure("test-32="+x, () -> {
+                for (int i = 0; i < 500000; i++) {
+                    THash.murmurHash32(TByte.getBytes(uniqueId.nextNumber()), 0, 8, 0);
+                }
+            });
+
+            TEnv.sleep(100);
+
+
+            TEnv.measure("test-64="+x, () -> {
+                for (int i = 0; i < 500000; i++) {
+                    THash.murmurHash128(TByte.getBytes(uniqueId.nextNumber()), 0, 8, 0);
+                }
+            });
+
+            TEnv.sleep(100);
+
+            System.out.println("======================================================================");
+        }
+    }
+}
