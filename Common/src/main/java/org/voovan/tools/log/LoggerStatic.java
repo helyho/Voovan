a0c9382a8931ac3c80da05775dce34fa9fb47a0e
helyho <helyho@gmail.com>
2020-04-25 12:13:11 +0800

    imp: TString 和 日志系统优化

--- Common/src/main/java/org/voovan/tools/log/LoggerStatic.java
+++ Common/src/main/java/org/voovan/tools/log/LoggerStatic.java
@@ -3,6 +3,10 @@ package org.voovan.tools.log;
 import org.voovan.tools.TObject;
 import org.voovan.tools.TProperties;
 import org.voovan.tools.TString;
+import static org.voovan.tools.TString.TOKEN_PREFIX_REGEX;
+import static org.voovan.tools.TString.TOKEN_SUFFIX_REGEX;
+import static org.voovan.tools.TString.TOKEN_PREFIX;
+import static org.voovan.tools.TString.TOKEN_SUFFIX;
 
 /**
  * 静态对象类
@@ -17,24 +21,30 @@ public class LoggerStatic {
 	private static long		startTimeMillis	= System.currentTimeMillis();
 
 	public final static String LOG_LEVEL = "ALL";
-	public final static String LOG_FILE = "{{WorkDir}}/logs/sysout.{{D}}.log";
+	public final static String LOG_FILE = "<WorkDir>/logs/sysout.<D>.log";
 	public final static String LOG_TYPE = "STDOUT";
-	public final static String DEFAULT_LOG_TEMPLATE = "--------------------------------------------------------------------------------------------------------------------------------------------------" +
-			"{{n}}[{{P}}] [{{D}}] [Thread:{{T}}] [Time:{{R}}] ({{F}}:{{L}}) {{n}}" +
+	public static String DEFAULT_LOG_TEMPLATE =
 			"--------------------------------------------------------------------------------------------------------------------------------------------------" +
-			"{{n}}{{I}}{{n}}{{n}}";
+			"<n>[<P>] [<D>] [Thread:<T>] [Time:<R>] (<F>:<L>) <n>" +
+			"--------------------------------------------------------------------------------------------------------------------------------------------------" +
+			"<n><I><n><n>";
+	static {
+		DEFAULT_LOG_TEMPLATE = DEFAULT_LOG_TEMPLATE.replace("<",TOKEN_PREFIX).replace(">",TOKEN_SUFFIX);
+	}
+
 	public final static String LOG_TEMPLATE = LoggerStatic.getLogConfig("LogTemplate", LoggerStatic.DEFAULT_LOG_TEMPLATE);
 
-	public static boolean HAS_COLOR = TString.regexMatch(LOG_TEMPLATE, "\\{\\{[FB][0-7D]\\}\\}") > 0;
-	public static boolean HAS_STACK = TString.regexMatch(LOG_TEMPLATE, "\\{\\{SI\\}\\}") > 0 || TString.regexMatch(LOG_TEMPLATE, "\\{\\{L\\}\\}") > 0 ||
-			TString.regexMatch(LOG_TEMPLATE, "\\{\\{M\\}\\}") > 0 ||
-			TString.regexMatch(LOG_TEMPLATE, "\\{\\{F\\}\\}") > 0 ||
-			TString.regexMatch(LOG_TEMPLATE, "\\{\\{C\\}\\}") > 0;
+	public static boolean HAS_COLOR = 	TString.regexMatch(LOG_TEMPLATE, TOKEN_PREFIX_REGEX + "[FB][0-7D]"	+ TOKEN_SUFFIX_REGEX) > 0;
+	public static boolean HAS_STACK = 	TString.regexMatch(LOG_TEMPLATE, TOKEN_PREFIX_REGEX + "SI"	+ TOKEN_SUFFIX_REGEX) > 0 ||
+										TString.regexMatch(LOG_TEMPLATE, TOKEN_PREFIX_REGEX + "L" 	+ TOKEN_SUFFIX_REGEX) > 0 ||
+										TString.regexMatch(LOG_TEMPLATE, TOKEN_PREFIX_REGEX + "M" 	+ TOKEN_SUFFIX_REGEX) > 0 ||
+										TString.regexMatch(LOG_TEMPLATE, TOKEN_PREFIX_REGEX + "F" 	+ TOKEN_SUFFIX_REGEX) > 0 ||
+										TString.regexMatch(LOG_TEMPLATE, TOKEN_PREFIX_REGEX + "C" 	+ TOKEN_SUFFIX_REGEX) > 0;
 
-	public static boolean HAS_LEVEL = TString.regexMatch(LOG_TEMPLATE, "\\{\\{P\\}\\}") > 0;
-	public static boolean HAS_DATE = TString.regexMatch(LOG_TEMPLATE, "\\{\\{D\\}\\}") > 0;
-	public static boolean HAS_THREAD = TString.regexMatch(LOG_TEMPLATE, "\\{\\{T\\}\\}") > 0;
-	public static boolean HAS_RUNTIME = TString.regexMatch(LOG_TEMPLATE, "\\{\\{R\\}\\}") > 0;
+						public static boolean HAS_LEVEL 	= TString.regexMatch(LOG_TEMPLATE, 	TOKEN_PREFIX_REGEX+"P"+TOKEN_SUFFIX_REGEX) > 0;
+	public static boolean HAS_DATE 		= TString.regexMatch(LOG_TEMPLATE, 	TOKEN_PREFIX_REGEX + "D" + TOKEN_SUFFIX_REGEX) > 0;
+	public static boolean HAS_THREAD 	= TString.regexMatch(LOG_TEMPLATE, 	TOKEN_PREFIX_REGEX + "T" + TOKEN_SUFFIX_REGEX) > 0;
+	public static boolean HAS_RUNTIME 	= TString.regexMatch(LOG_TEMPLATE, 	TOKEN_PREFIX_REGEX + "R" + TOKEN_SUFFIX_REGEX) > 0;
 
 	/**
 	 * 获取启动时间信息
