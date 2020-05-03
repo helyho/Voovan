abd3aea41ad6349f165eb17451f51bda0dc2e8f8
helyho <helyho@gmail.com>
2020-05-03 13:15:25 +0800

    fix: 修复 build_env.sh 的 bug

--- build_env.sh
+++ build_env.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+VOOVAN_VERSION=4.2.4
+
 jlink --no-header-files --no-man-pages --compress=2 --strip-debug --module-path $JAVA_HOME/jmods \
  --add-modules java.base,java.compiler,java.instrument,java.management,java.sql,jdk.attach,jdk.unsupported \
  --output launcher/jvm
 
+
+mvn package -Dmaven.test.skip=true -Dmaven.javadoc.skip=true
 mkdir launcher/lib
-cp All/voovan-framework.jar launcher/lib/
+cp All/target/voovan-framework-$VOOVAN_VERSION.jar launcher/lib/
 
 mkdir launcher/conf
 cp -r Web/conf/error-page Web/conf/error.json launcher/conf/
@@ -14,14 +18,14 @@ mkdir launcher/logs
 
 mkdir launcher/WEBAPP
 
-echo "#\!/bin/sh\n./jvm/bin/java -Djdk.attach.allowAttachSelf=true -jar lib/voovan-framework.jar" > launcher/start.sh
+echo "#\!/bin/sh\n./jvm/bin/java -Djdk.attach.allowAttachSelf=true -jar lib/voovan-framework-$VOOVAN_VERSION.jar" > launcher/start.sh
 chmod +x launcher/start.sh
 
 echo "#\!/bin/sh\ncat logs/.pid | xargs kill" > launcher/stop.sh
 chmod +x launcher/stop.sh
 
-echo "#\!/bin/sh\ncat logs/.pid | xargs kill -9" > launcher/forceStop.sh
-chmod +x launcher/forceStop.sh
+echo "#\!/bin/sh\ncat logs/.pid | xargs kill -9" > launcher/shutdown.sh
+chmod +x launcher/shutdown.sh
 
 
 
