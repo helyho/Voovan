867ebdd7349c3a2b75e97be00930dcf901d7068d
helyho <helyho@gmail.com>
2020-05-09 19:20:01 +0800

    add: 新增 Attribute 注解

---
+++ Web/src/main/java/org/voovan/http/server/module/annontationRouter/annotation/Attribute.java
@@ -0,0 +1,19 @@
+package org.voovan.http.server.module.annontationRouter.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 注解路由 Session
+ *
+ * @author: helyho
+ * Voovan Framework.
+ * WebSite: https://github.com/helyho/Voovan
+ * Licence: Apache v2 License
+ */
+@Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Attribute {
+    String value() default "";
+    boolean isRequire() default true;
+}
