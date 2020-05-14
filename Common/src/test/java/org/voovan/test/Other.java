9882223e2aa29cc1afd08984fbfb9f2f672724c2
helyho <helyho@gmail.com>
2020-05-15 03:53:37 +0800

    add: 新增 JSON.replaceNode 用于替换属性

--- Common/src/test/java/org/voovan/test/Other.java
+++ Common/src/test/java/org/voovan/test/Other.java
@@ -36,10 +36,20 @@ public class Other {
     private int orange = 10;
 
     public static void main(String[] args) throws Exception {
-        TreeMap<String, Object> m = new TreeMap<String, Object>(String.CASE_INSENSITIVE_ORDER);
-
-        m.put("cookie", 1);
-
-        System.out.println(m.get("Cookie"));
+       String xx = "{\n" +
+               "  \"userId\": 666,\n" +
+               "  \"userId\": 666,\n" +
+               "  \"userId\": 666,\n" +
+               "  \"userId\": null,\n" +
+               "  \"accountName\": \"Default\",\n" +
+               "  \"type\": 0,\n" +
+               "  \"userId\": \"666\",\n" +
+               "  \"userId\": null\n" +
+               "}";
+        System.out.println(xx);
+        System.out.println("rn: " +JSON.removeNullNode(xx));
+        System.out.println("r: " + JSON.removeNode(xx, "userId"));
+       String mm = JSON.replaceNode(xx, "userId", TObject.asMap("userId", 123123));
+       System.out.println("rr: " + mm);
     }
 }
