4e11fd24719991a349ebb94225176ee74663d8f0
helyho <helyho@gmail.com>
2020-05-15 14:56:38 +0800

    imp: TString 优化

--- Common/src/main/java/org/voovan/tools/TString.java
+++ Common/src/main/java/org/voovan/tools/TString.java
@@ -754,28 +754,27 @@ public class TString {
 		return (T) toObject(value, type, false);
 	}
 
-	public static String[] chars = new String[]{"0", "1", "2", "3", "4", "5",
-			"6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
-			"J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
-			"W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i",
-			"j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-			"w", "x", "y", "z"};
+	public static char[] chars = new char[]{'0', '1', '2', '3', '4', '5',
+			'6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+			'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+			'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+			'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+			'w', 'x', 'y', 'z'};
 
-	public static Map<String, Integer> unChars = new HashMap<String, Integer>();
+	public static Map<Character, Integer> unChars = new HashMap<Character, Integer>();
 
 	static {
 		initUnchars();
 	}
 
-	public static void setChars(String[] argChars) {
+	public static void setChars(char[] argChars) {
 		argChars = chars;
 		initUnchars();
 	}
 
 	private static void initUnchars() {
 		for(int i=0;i<chars.length;i++) {
-			String oneChar = chars[i];
-			unChars.put(oneChar,i);
+			unChars.put(chars[i],i);
 		}
 	}
 
