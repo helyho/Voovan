4f0206357aa8470a9794525bc0ac8dee8152cbb9
helyho <helyho@gmail.com>
2020-05-15 12:19:17 +0800

    imp: TString 可定制 char 用户转换radix****Convert 方法砖混

--- Common/src/main/java/org/voovan/tools/TString.java
+++ Common/src/main/java/org/voovan/tools/TString.java
@@ -720,13 +720,23 @@ public class TString {
 			"j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
 			"w", "x", "y", "z"};
 
-	public static Map<String, Integer> unChars = TObject.asMap('0', 0, '1', 1, '2', 2, '3', 3, '4', 4, '5', 5,
-			'6', 6, '7', 7, '8', 8, '9', 9, 'A', 10, 'B', 11, 'C', 12, 'D', 13, 'E', 14, 'F', 15, 'G', 16, 'H', 17, 'I', 18,
-			'J', 19, 'K', 20, 'L', 21, 'M', 22, 'N', 23, 'O', 24, 'P', 25, 'Q', 26, 'R', 27, 'S', 28, 'T', 29, 'U', 30, 'V', 31,
-			'W', 32, 'X', 33, 'Y', 34, 'Z', 35, 'a', 36, 'b', 37, 'c', 38, 'd', 39, 'e', 40, 'f', 41, 'g', 42, 'h', 43, 'i', 44,
-			'j', 45, 'k', 46, 'l', 47, 'm', 48, 'n', 49, 'o', 50, 'p', 51, 'q', 52, 'r', 53, 's', 54, 't', 55, 'u', 56, 'v', 57,
-			'w', 58, 'x', 59, 'y', 60, 'z', 61);
+	public static Map<String, Integer> unChars = new HashMap<String, Integer>();
 
+	static {
+		initUnchars();
+	}
+
+	public static void setChars(String[] argChars) {
+		argChars = chars;
+		initUnchars();
+	}
+
+	private static void initUnchars() {
+		for(int i=0;i<chars.length;i++) {
+			String oneChar = chars[i];
+			unChars.put(oneChar,i);
+		}
+	}
 
 	public static String radixConvert(long num, int radix) {
 
