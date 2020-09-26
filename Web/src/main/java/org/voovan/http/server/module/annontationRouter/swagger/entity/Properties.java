e112a2a3456e533228b92641643f3d0cb0d02014
helyho <helyho@gmail.com>
2020-09-26 18:21:30 +0800

    imp: swagger 优化

--- Web/src/main/java/org/voovan/http/server/module/annontationRouter/swagger/entity/Properties.java
+++ Web/src/main/java/org/voovan/http/server/module/annontationRouter/swagger/entity/Properties.java
@@ -14,31 +14,31 @@ import java.util.TreeMap;
  * Licence: Apache v2 License
  */
 public class Properties {
-    public transient Property property;
+    public transient Schema schema;
 
     //properties 属性名->SchemaItem
-    Map<String, Property> properties;
+    Map<String, Schema> properties;
 
     @Serialization("$ref")
     private String ref;
 
-    public Property getProperty() {
-        return property;
+    public Schema getProperty() {
+        return schema;
     }
 
-    public void setProperty(Property property) {
-        this.property = property;
+    public void setProperty(Schema schema) {
+        this.schema = schema;
     }
 
-    public Map<String, Property> getProperties() {
+    public Map<String, Schema> getProperties() {
         if(properties == null) {
-            properties = new TreeMap<String, Property>();
+            properties = new TreeMap<String, Schema>();
         }
 
         return properties;
     }
 
-    public void setProperties(Map<String, Property> properties) {
+    public void setProperties(Map<String, Schema> properties) {
         this.properties = properties;
     }
 
