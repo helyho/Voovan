928c39046616a6a9ffd93c651acb1ef0260654af
helyho <helyho@gmail.com>
2020-12-07 19:26:28 +0800

    imp: Header 优化

--- Web/src/main/java/org/voovan/http/message/packet/Header.java
+++ Web/src/main/java/org/voovan/http/message/packet/Header.java
@@ -20,10 +20,6 @@ import java.util.TreeMap;
 public class Header {
 	private static FastThreadLocal<StringBuilder> THREAD_STRING_BUILDER = FastThreadLocal.withInitial(()->new StringBuilder(512));
 
-	private String contentType;
-	private String contentLength;
-	private String contentEncoding;
-	private String transferEncoding;
 	private Map<String, String> headers;
 
 	/**
@@ -44,10 +40,6 @@ public class Header {
 	}
 
 	public void setHeaders(Map<String, String> headers) {
-		contentType = null;
-		contentLength = null;
-		contentEncoding = null;
-		transferEncoding = null;
 		this.headers = headers;
 	}
 
@@ -57,28 +49,7 @@ public class Header {
 	 * @return 移除的header 的 name
 	 */
 	public String remove(String header){
-		String ret = null;
-		switch (header) {
-			case HttpStatic.CONTENT_TYPE_STRING:
-				ret = this.headers.remove(HttpStatic.CONTENT_TYPE_STRING);
-				contentType = null;
-				break;
-			case HttpStatic.CONTENT_ENCODING_STRING:
-				ret = this.headers.remove(HttpStatic.CONTENT_ENCODING_STRING);
-				contentEncoding = null;
-				break;
-			case HttpStatic.CONTENT_LENGTH_STRING:
-				ret = this.headers.remove(HttpStatic.CONTENT_LENGTH_STRING);
-				contentLength = null;
-				break;
-			case HttpStatic.TRANSFER_ENCODING_STRING:
-				ret = this.headers.remove(HttpStatic.TRANSFER_ENCODING_STRING);
-				transferEncoding = null;
-				break;
-			default:
-				ret = headers.remove(header);
-		}
-		return ret;
+		return headers.remove(header);
 	}
 
 	/**
@@ -87,16 +58,7 @@ public class Header {
 	 * @return 是否存在
 	 */
 	public boolean contain(String header){
-		boolean ret = false;
-		switch (header) {
-			case HttpStatic.CONTENT_TYPE_STRING 	 :  ret = get(header) != null;  break;
-			case HttpStatic.CONTENT_LENGTH_STRING 	 :  ret = get(header) != null;  break;
-			case HttpStatic.CONTENT_ENCODING_STRING  :  ret = get(header) != null;  break;
-			case HttpStatic.TRANSFER_ENCODING_STRING :  ret = get(header) != null;  break;
-			default: ret = headers.containsKey(header);
-		}
-
-		return ret;
+		return headers.containsKey(header);
 	}
 
 	/**
@@ -105,29 +67,7 @@ public class Header {
 	 * @return header 的值
 	 */
 	public String get(String header){
-		String ret = null;
-		boolean tryGetFromMap = false;
-		switch (header) {
-			case HttpStatic.CONTENT_TYPE_STRING 	 : 	{
-				ret = contentType == null ? contentType = headers.get(header) : contentType;
-				break;
-			}
-			case HttpStatic.CONTENT_LENGTH_STRING 	 : 	{
-				ret = contentLength == null ? contentLength = headers.get(header) : contentLength;
-				break;
-			}
-			case HttpStatic.CONTENT_ENCODING_STRING  : 	{
-				ret = contentEncoding == null ? contentEncoding = headers.get(header) : contentEncoding;
-				break;
-			}
-			case HttpStatic.TRANSFER_ENCODING_STRING : 	{
-				ret = transferEncoding == null ? transferEncoding = headers.get(header) : transferEncoding;
-				break;
-			}
-			default: ret = headers.get(header);
-		}
-
-		return ret;
+		return headers.get(header);
 	}
 
 	/**
@@ -137,14 +77,7 @@ public class Header {
 	 * @return header 的 name
 	 */
 	public String put(String header,String value){
-		switch (header) {
-			case HttpStatic.CONTENT_TYPE_STRING 	 :  this.contentType 		= value; break;
-			case HttpStatic.CONTENT_LENGTH_STRING 	 : 	this.contentLength 		= value; break;
-			case HttpStatic.CONTENT_ENCODING_STRING  : 	this.contentEncoding 	= value; break;
-			case HttpStatic.TRANSFER_ENCODING_STRING : 	this.transferEncoding 	= value; break;
-			default : headers.put(header,value);
-		}
-		return value;
+		return headers.put(header,value);
 	}
 
 	/**
@@ -152,9 +85,7 @@ public class Header {
 	 * @param valueMap Header 的 Map 形式
 	 */
 	public void putAll(Map<String, String> valueMap){
-		for(Entry<String, String> entry : valueMap.entrySet()) {
-			put(entry.getKey(), entry.getValue());
-		}
+		headers.putAll(valueMap);
 	}
 
 	/**
@@ -167,10 +98,6 @@ public class Header {
 
 	public Header copyFrom(Header otherHeader) {
 		this.putAll(otherHeader.getHeaders());
-		this.put(HttpStatic.CONTENT_TYPE_STRING, otherHeader.get(HttpStatic.CONTENT_TYPE_STRING));
-		this.put(HttpStatic.CONTENT_LENGTH_STRING, otherHeader.get(HttpStatic.CONTENT_LENGTH_STRING));
-		this.put(HttpStatic.CONTENT_ENCODING_STRING, otherHeader.get(HttpStatic.CONTENT_ENCODING_STRING));
-		this.put(HttpStatic.TRANSFER_ENCODING_STRING, otherHeader.get(HttpStatic.TRANSFER_ENCODING_STRING));
 		return this;
 	}
 
@@ -178,45 +105,13 @@ public class Header {
 	 * 清空头
 	 */
 	public void clear(){
-		contentType = null;
-		contentLength = null;
-		contentEncoding = null;
-		transferEncoding = null;
-		headers = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
+		headers.clear();
 	}
 
 	@Override
 	public String toString(){
 		StringBuilder headerContent = THREAD_STRING_BUILDER.get();
 		headerContent.setLength(0);
-
-		if(contentType!=null) {
-			headerContent.append(HttpStatic.CONTENT_TYPE_STRING);
-			headerContent.append(HttpStatic.HEADER_SPLITER_STRING);
-			headerContent.append(contentType);
-			headerContent.append(HttpStatic.LINE_MARK_STRING);
-		}
-
-		if(contentEncoding!=null) {
-			headerContent.append(HttpStatic.CONTENT_ENCODING_STRING);
-			headerContent.append(HttpStatic.HEADER_SPLITER_STRING);
-			headerContent.append(contentEncoding);
-			headerContent.append(HttpStatic.LINE_MARK_STRING);
-		}
-
-		if(contentLength!=null) {
-			headerContent.append(HttpStatic.CONTENT_LENGTH_STRING);
-			headerContent.append(HttpStatic.HEADER_SPLITER_STRING);
-			headerContent.append(contentLength);
-			headerContent.append(HttpStatic.LINE_MARK_STRING);
-		}
-
-		if(transferEncoding!=null) {
-			headerContent.append(HttpStatic.TRANSFER_ENCODING_STRING);
-			headerContent.append(HttpStatic.HEADER_SPLITER_STRING);
-			headerContent.append(transferEncoding);
-			headerContent.append(HttpStatic.LINE_MARK_STRING);
-		}
 
 		for(Entry<String,String> headerItemEntry : this.headers.entrySet()){
 			String key = headerItemEntry.getKey();
