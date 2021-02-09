2f34692cba41072abc03cfa051e87196336317d4
helyho <helyho@gmail.com>
2021-02-09 19:01:28 +0800

    imp: 新增 WebServer 本地控制台

--- Common/src/main/java/org/voovan/tools/hotswap/Hotswaper.java
+++ Common/src/main/java/org/voovan/tools/hotswap/Hotswaper.java
@@ -175,7 +175,7 @@ public class Hotswaper {
 
             ClassDefinition classDefinition = new ClassDefinition(clazz, classBytes);
             try {
-                Logger.info("[HOTSWAP] " + TDateTime.now() + " class->" + clazz.getCanonicalName() + " will reload.");
+                Logger.info("[HOTSWAP] " + TDateTime.now() + clazz.getName() + " will reload.");
                 TEnv.instrumentation.redefineClasses(classDefinition);
 
                 for(Consumer<Class> wather : WATCHERS){
