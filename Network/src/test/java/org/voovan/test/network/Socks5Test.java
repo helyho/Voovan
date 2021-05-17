2a24157afe2dbfe0f71c240a497988c02ee860e7
helyho <helyho@gmail.com>
2021-05-17 14:54:46 +0800

    imp: Socks5Plugin 优化

--- Network/src/test/java/org/voovan/test/network/Socks5Test.java
+++ Network/src/test/java/org/voovan/test/network/Socks5Test.java
@@ -19,15 +19,15 @@ public class Socks5Test {
 	public static void main(String[] args) throws Exception {
 		TcpSocket socket = new TcpSocket("127.0.0.1", 1080, 5000 * 60, 1);
 		socket.pluginChain()
-				.add(new Socks5Plugin(new InetSocketAddress("www.google.com", 443)))
-				.add(new SSLPlugin());
+				.add(new Socks5Plugin(new InetSocketAddress("www.facebook.com", 80)));
+//				.add(new SSLPlugin());
 		socket.syncStart();
 
 
 		IoSession session = socket.getSession();
 
 		String reqData = "GET / HTTP/1.1\r\n" +
-				"Host: www.google.com\r\n" +
+				"Host: www.facebook.com\r\n" +
 				"Connection: keep-alive\r\n" +
 				"Cache-Control: max-age=0\r\n" +
 				"Upgrade-Insecure-Requests: 1\r\n" +
@@ -35,7 +35,9 @@ public class Socks5Test {
 				"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n" +
 				"Accept-Language: zh-CN,zh;q=0.9,en;q=0.8\r\n" +
 				"Cookie: _ga=GA1.1.383879526.1585421546; _ga_NSJH3FW0LK=GS1.1.1620788011.10.1.1620788300.0\r\n\r\n";
+
 		ByteBuffer byteBuffer = ByteBuffer.allocate(reqData.length());
+
 		byteBuffer.put(reqData.getBytes());
 		byteBuffer.flip();
 		session.send(byteBuffer, true);
