14e095a4230673090d70a6c4d8d6aa0c34473cec
helyho <helyho@gmail.com>
2021-05-20 15:51:44 +0800

    imp: 新增 JSON 两个转换器

---
+++ Common/src/main/java/org/voovan/tools/reflect/convert/ToDateString.java
@@ -0,0 +1,25 @@
+package org.voovan.tools.reflect.convert;
+
+import org.voovan.tools.TDateTime;
+
+import java.math.BigDecimal;
+
+/**
+ * 时间戳转换成字符串形式的时间格式
+ *
+ * @author: helyho
+ * Voovan Framework.
+ * WebSite: https://github.com/helyho/Voovan
+ * Licence: Apache v2 License
+ */
+public class ToDateString<Long> implements Convert<Long, String> {
+
+    @Override
+    public String convert(String name, Long parameter) {
+        if(parameter == null) {
+            return null;
+        }
+
+        return TDateTime.timestamp((long) parameter);
+    }
+}
