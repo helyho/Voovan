a11f378036487d84ccbcf3602dcd4686d19988f2
helyho <helyho@gmail.com>
2021-07-01 12:00:06 +0800

    doc: 文档更新

--- Network/src/main/java/org/voovan/network/IoPlugin.java
+++ Network/src/main/java/org/voovan/network/IoPlugin.java
@@ -15,7 +15,7 @@ import java.nio.ByteBuffer;
  */
 public interface IoPlugin {
     /**
-     * 初始化插件信息<br/>
+     * 初始化插件信息<br>
      * 在 SocketContext.syncStart/acceptStart  内触发
      * @param session IoSession 会话对象
      */
@@ -29,7 +29,7 @@ public interface IoPlugin {
     public void prepare(IoSession session);
 
     /**
-     * 获取默认的读取 ByteBufferChannel<br/>
+     * 获取默认的读取 ByteBufferChannel<br>
      * 默认会使用这个方法返回的 ByteBufferChannel 来接收 os.tcp 缓冲区的数据.
      * 例如: ssl 使用了 SSLParser中的 ByteBufferChannel 来接手数据, 并在 unwarp 中转换到 IoSession.getReadByteBufferChannel 中来支持框架后续行为
      * @param session IoSession 会话对象
@@ -39,7 +39,7 @@ public interface IoPlugin {
 
     /**
      * 数据打包
-     * IoSession.send 内触发<br/>
+     * IoSession.send 内触发<br>
      * @param session IoSession 会话对象
      * @param byteBuffer 需要发送的数据
      * @return 转换给下一个 plugin 的 ByteBuffer 对象
@@ -47,14 +47,14 @@ public interface IoPlugin {
     public ByteBuffer warp(IoSession session, ByteBuffer byteBuffer);
 
     /**
-     * 数据解包<br/>
+     * 数据解包<br>
      * SocketSelector.loadAndPrepare 内触发
      * @param session IoSession 会话对象
      */
     public void unwarp(IoSession session);
 
     /**
-     * 过滤器释放<br/>
+     * 过滤器释放<br>
      * IoSession.release  内触发
      * @param session IoSession 会话对象
      */
