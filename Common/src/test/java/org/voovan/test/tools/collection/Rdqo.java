2c815aaa287fe60ff2835bbfb7afd8f522b35fe7
helyho <helyho@gmail.com>
2021-08-25 00:32:50 +0800

    imp: RocksDelayQueue 第一版

---
+++ Common/src/test/java/org/voovan/test/tools/collection/Rdqo.java
@@ -0,0 +1,35 @@
+package org.voovan.test.tools.collection;
+
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Class name
+ *
+ * @author: helyho
+ * Voovan Framework.
+ * WebSite: https://github.com/helyho/Voovan
+ * Licence: Apache v2 License
+ */
+public class Rdqo implements Delayed {
+    long delayTime;
+    Object value;
+
+    public Rdqo() {
+    }
+
+    public Rdqo(long delayTime, Object value) {
+        this.delayTime = delayTime;
+        this.value = value;
+    }
+
+
+    public long getDelay(TimeUnit unit) {
+        return unit.convert(delayTime, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public int compareTo(Delayed o) {
+        return 0;
+    }
+}
