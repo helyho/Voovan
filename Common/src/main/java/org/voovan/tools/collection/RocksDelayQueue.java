70bdd2fd85c4f0352bfc0de23580564864a812d7
helyho <helyho@gmail.com>
2021-08-25 00:32:50 +0800

    imp: RocksDelayQueue 第一版

---
+++ Common/src/main/java/org/voovan/tools/collection/RocksDelayQueue.java
@@ -0,0 +1,274 @@
+package org.voovan.tools.collection;
+
+import org.rocksdb.RocksIterator;
+import org.voovan.tools.TByte;
+import org.voovan.tools.TDateTime;
+import org.voovan.tools.TEnv;
+import org.voovan.tools.TString;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * RocksDB 的 DelayQueue 封装 <br>
+ * 最小延时单位: 秒
+ * @author: helyho
+ * Voovan Framework.
+ * WebSite: https://github.com/helyho/Voovan
+ * Licence: Apache v2 License
+ */
+public class RocksDelayQueue<E extends Delayed> implements Queue<E> {
+
+    private RocksMap root;
+    private RocksMap<String, E> container;
+    private ConcurrentLinkedQueue<String> queueCache = new ConcurrentLinkedQueue<>();
+    private volatile String lastKey;
+
+    public RocksDelayQueue(RocksMap rocksMap, String name) {
+        this.root = rocksMap;
+        this.container = rocksMap.duplicate(name);
+        this.container.getReadOptions().setTotalOrderSeek(true);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                RocksMap.RocksMapIterator iterator0 = null;
+                synchronized (container) {
+                    String fromKey = lastKey == null ? null : container.isKeyExists(lastKey) ? lastKey : null;
+                    String toKey = lastKey == null ? TString.radixConvert(System.currentTimeMillis(), 62) : null;
+                    iterator0 = container.iterator(fromKey, toKey, fromKey == null ? 0 : 1, 0);
+                }
+
+                try (RocksMap.RocksMapIterator iterator = iterator0) {
+                    RocksMap.RocksMapEntry rocksMapEntry = null;
+                    while ((rocksMapEntry = iterator.next()) != null) {
+                        String key = rocksMapEntry.getKey().toString();
+                        lastKey = key;
+                        queueCache.add(key);
+                    }
+                }
+
+            }
+        }, 1000, 1000);
+    }
+
+    public RocksMap getRoot() {
+        return root;
+    }
+
+    public RocksMap<String, E> getContainer() {
+        return container;
+    }
+
+    private synchronized String offerSeq(long delayMilliSecond){
+        String ret = TString.radixConvert((System.currentTimeMillis() + delayMilliSecond)/1000, 62) + "-" + TString.radixConvert(TDateTime.currentTimeNanos(), 62);
+        return ret;
+    }
+
+    private synchronized String currentMilliTime(){
+        String ret =  TString.radixConvert((System.currentTimeMillis())/1000, 62);
+        return ret;
+    }
+
+
+    @Override
+    public boolean add(E e) {
+        container.put(offerSeq(e.getDelay(TimeUnit.MILLISECONDS)), e);
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        c.stream().forEach(e->add(e));
+        return true;
+    }
+
+
+
+    @Override
+    public int size() {
+        return container.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return container.isEmpty();
+    }
+
+    @Override
+    public boolean offer(E e) {
+        return add(e);
+    }
+
+    @Override
+    public E remove() {
+        return poll();
+    }
+
+    public E take(long timeout, TimeUnit unit) throws TimeoutException {
+        E e = poll();
+
+        long time = TimeUnit.SECONDS.convert(timeout, unit);
+        while(time>0) {
+            if (e == null) {
+                e = poll();
+            }
+
+            if(e!=null) {
+                break;
+            }
+
+            time--;
+            synchronized (this) {
+                try {
+                    this.wait(1000);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        if (e == null) {
+            throw new TimeoutException();
+        }
+
+        return e;
+
+    }
+
+    ArrayList mmm = new ArrayList();
+
+    @Override
+    public E poll() {
+        //RocksDB 迭代器多线程的并发问题, 有可能已经在其他线程 poll 过这个 key, 所以这里使用循环获取
+        while(true) {
+            String seq = queueCache.poll();
+            if (seq != null) {
+                E e = container.remove(seq);
+                if(e==null) {
+                    continue;
+                }
+                return e;
+            } else {
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public E element() {
+        E e = poll();
+        if(e == null) {
+            throw new NoSuchElementException();
+        }
+        return e;
+    }
+
+    public Collection<E> get(long second) {
+        return container.startWith(currentMilliTime()).values();
+    }
+
+    @Override
+    public E peek() {
+        return container.get(queueCache.peek());
+    }
+
+    @Override
+    public void clear() {
+        container.clear();
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new RocksQueueIterator(this);
+    }
+
+    @Override
+    public Object[] toArray() {
+        return container.values().toArray();
+
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return (T[]) container.values().toArray(a);
+    }
+
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void flush(boolean sync, boolean allowStall){
+        container.flush(sync, allowStall);
+    }
+
+    public void flush(boolean allowStall){
+        flush(true, allowStall);
+    }
+
+    public void flush(){
+        flush(true, false);
+    }
+
+    @Override
+    public String toString() {
+        return container.toString();
+    }
+
+
+    public class RocksQueueIterator<E extends Delayed> implements Iterator<E>, Closeable {
+        RocksMap.RocksMapIterator rocksMapIterator;
+
+        protected RocksQueueIterator(RocksDelayQueue<E> queue) {
+            this.rocksMapIterator = queue.getContainer().iterator();
+        }
+
+        @Override
+        public void close() throws IOException {
+            rocksMapIterator.close();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return rocksMapIterator.hasNext();
+        }
+
+        public byte[] nextBytes() {
+            return rocksMapIterator.valueBytes();
+        }
+
+        public E next() {
+            return (E) rocksMapIterator.value();
+        }
+
+        public boolean isValid(){
+            return rocksMapIterator.isValid();
+        }
+    }
+}
---
+++ Common/src/test/java/org/voovan/test/tools/collection/RocksDelayQueueUnit.java
@@ -0,0 +1,141 @@
+package org.voovan.test.tools.collection;
+
+import junit.framework.TestCase;
+import org.voovan.tools.TDateTime;
+import org.voovan.tools.TEnv;
+import org.voovan.tools.collection.RocksDelayQueue;
+import org.voovan.tools.collection.RocksMap;
+import org.voovan.tools.collection.RocksQueue;
+import org.voovan.tools.event.EventRunnerGroup;
+import org.voovan.tools.json.JSON;
+import org.voovan.tools.log.Logger;
+import org.voovan.tools.serialize.ProtoStuffSerialize;
+import org.voovan.tools.serialize.TSerialize;
+
+import java.util.concurrent.Delayed;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 类文字命名
+ *
+ * @author: helyho
+ * voovan Framework.
+ * WebSite: https://github.com/helyho/voovan
+ * Licence: Apache v2 License
+ */
+public class RocksDelayQueueUnit extends TestCase {
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        TSerialize.SERIALIZE = new ProtoStuffSerialize();
+    }
+
+
+    public void testRocksQueue() {
+        String cfName = "testdb1000";
+        RocksMap rocksMap = new RocksMap(cfName);
+        RocksDelayQueue rocksQueue = new RocksDelayQueue(rocksMap, "testDelayQueue");
+        rocksQueue.clear();
+
+        rocksQueue.add(new Rdqo(-1, "asdfasdfasdf"));
+        rocksQueue.add(new Rdqo(1, "asdfasdfasdf"));
+        rocksQueue.add(new Rdqo(2, "asdfasdfasdf"));
+        rocksQueue.add(new Rdqo(3, "asdfasdfasdf"));
+        rocksQueue.add(new Rdqo(4, "asdfasdfasdf"));
+        rocksQueue.add(new Rdqo(5, "asdfasdfasdf"));
+        rocksQueue.add(new Rdqo(6, "asdfasdfasdf"));
+        rocksQueue.add(new Rdqo(7, "asdfasdfasdf"));
+
+        for(int i=0;i<10;i++) {
+            if(i==3){
+                rocksQueue.add(new Rdqo(-20, "asdfasdfasdf"));
+            }
+            System.out.println(TDateTime.now() + " " + JSON.toJSON(rocksQueue.poll()));
+            TEnv.sleep(1000);
+        }
+
+        TEnv.sleep(1000);
+
+    }
+
+    public void testRocksQueueTake() {
+        String cfName = "testdb1000";
+        RocksMap rocksMap = new RocksMap(cfName);
+        RocksDelayQueue rocksQueue = new RocksDelayQueue(rocksMap, "testDelayQueue");
+        rocksQueue.clear();
+
+        rocksQueue.add(new Rdqo(-3, "asdfasdfasdf"));
+        rocksQueue.add(new Rdqo(8, "asdfasdfasdf"));
+
+        System.out.println(TDateTime.now());
+        System.out.println("all data: " + JSON.toJSON(rocksQueue.getContainer().keySet()));
+        try {
+
+            Object value1 = rocksQueue.take(10, TimeUnit.SECONDS);
+            System.out.println("data1: " + JSON.toJSON(value1) + " " + TDateTime.now());
+            Object value2 = rocksQueue.take(10, TimeUnit.SECONDS);
+            System.out.println("data2: " + JSON.toJSON(value2) + " " + TDateTime.now());
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+        ;
+
+        TEnv.sleep(100000);
+    }
+
+    public void testConcurrentRocksQueue() {
+        String cfName = "testdb1000";
+        RocksMap rocksMap = new RocksMap(cfName);
+        RocksDelayQueue rocksQueue = new RocksDelayQueue(rocksMap, "testDelayQueue");
+        rocksQueue.clear();
+        System.out.println(rocksQueue.size());
+
+        EventRunnerGroup eventRunnerGroup = EventRunnerGroup.newInstance();
+
+        TEnv.measure("add cost: ", ()->{
+            for (int x = 0; x < 50; x++) {
+                int finalX = x;
+                eventRunnerGroup.addEvent(() -> {
+                            for (int i = 0; i < 10000; i++) {
+                                rocksQueue.add(new Rdqo(-1000, finalX * 10 + i));
+                            }
+                        }
+                );
+            }
+
+            eventRunnerGroup.await();
+        });
+
+
+        System.out.println(rocksQueue.size());
+
+        AtomicInteger pollCnt = new AtomicInteger();
+        TEnv.measure("poll cost: ", ()->{
+            for (int x = 0; x < 50; x++) {
+                int finalX = x;
+                eventRunnerGroup.addEvent(() -> {
+                            for (int i = 0; i < 10000; i++) {
+                                if(rocksQueue.poll()==null) {
+                                    Logger.info("null");
+                                    i--;
+                                } else {
+                                    pollCnt.getAndIncrement();
+                                }
+                            }
+                        }
+                );
+            }
+
+            eventRunnerGroup.await();
+        });
+
+        System.out.println(TDateTime.now() + " " + pollCnt.get());
+        System.out.println(rocksQueue.size());
+
+        TEnv.sleep(4000);
+        assertEquals(rocksQueue.size(), 0);
+    }
+}
