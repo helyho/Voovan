c585701df01c64e9d1fe7e4e2d808e221545cecd
helyho <helyho@gmail.com>
2021-11-13 03:05:22 +0000

    imp: Other 优化

--- Common/src/test/java/org/voovan/test/Other.java
+++ Common/src/test/java/org/voovan/test/Other.java
@@ -1,16 +1,6 @@
 package org.voovan.test;
 
-import org.junit.rules.TestRule;
-import org.voovan.tools.TEnv;
-import org.voovan.tools.json.JSON;
 import org.voovan.tools.log.Logger;
-
-import java.math.BigDecimal;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.net.Socket;
-import java.nio.ByteBuffer;
-import java.util.List;
 
 /**
  * none
@@ -23,7 +13,5 @@ import java.util.List;
 public class Other {
     public static void main(String[] args) throws Exception {
         Logger.infof(System.getenv("USER"));
-
-        TEnv.sleep(100);
     }
 }
