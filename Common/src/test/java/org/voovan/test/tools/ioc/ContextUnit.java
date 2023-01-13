7d32cdb6bb6ba4b95c15b5cb80416beff6411c4f
helyho <helyho@gmail.com>
2023-01-13 15:22:32 +0800

    imp: 优化

--- Common/src/test/java/org/voovan/test/tools/ioc/ContextUnit.java
+++ Common/src/test/java/org/voovan/test/tools/ioc/ContextUnit.java
@@ -15,9 +15,12 @@ public class ContextUnit {
     public static void main(String[] args) {
         Container container = Context.getDefaultContainer();
         IOC2 ioc2 = container.getByAnchor("IOC2", null);
+        System.out.println("=========ioc2=========");
         IOC3 ioc30 = container.get("IOC3Method", null);
+        System.out.println("=========ioc30=========");
         IOC3 ioc31 = container.getByAnchor("IOC3", null);
+        System.out.println("=========ioc31=========");
         IOC3 ioc32 = container.getByAnchor("IOC3", null);
-        System.out.println("1111");
+        System.out.println("=========ioc32=========");
     }
 }
