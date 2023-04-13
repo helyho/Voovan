80a33d28ead357b76a862ec4754e41a96f29cc4b
helyho <helyho@gmail.com>
2023-04-13 13:07:31 +0800

    add: 完善注释

--- Web/src/main/java/org/voovan/http/server/module/annontationRouter/swagger/annotation/ApiWrapResponse.java
+++ Web/src/main/java/org/voovan/http/server/module/annontationRouter/swagger/annotation/ApiWrapResponse.java
@@ -3,7 +3,8 @@ package org.voovan.http.server.module.annontationRouter.swagger.annotation;
 import java.lang.annotation.*;
 
 /**
- * Class name
+ * 用来包裹响应
+ *      有的时候会使用 Filter 来变更响应类型, 通过这个注解可以在 swagger 中变更响应类型的描述
  *
  * @author helyho
  * voovan Framework.
@@ -14,8 +15,10 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface ApiWrapResponse {
+    // 响应变更的目标类型
     Class value();
 
+    //真实响应在响应变更的新类型中的字段名称
     String field() default "";
 
 }
